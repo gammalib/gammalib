ce28f030fbc36d7abe4e1ac74d31c87387b59b10
knodlseder <>
2008-03-12 04:47:22 +0000

    include mathematical constants

--- src/GTools.hpp
+++ src/GTools.hpp
@@ -21,6 +21,14 @@
 
 /* __ Namespaces _________________________________________________________ */
 
+/* __ Constants __________________________________________________________ */
+const double pi      = 3.1415926535897931159979635;
+const double pihalf  = pi/2.0;
+const double twopi   = 2.0*pi;
+const double deg2rad = 0.0174532925199432954743717;
+const double rad2deg = 57.295779513082322864647722;
+const double ln10    = 2.3025850929940459010936138;
+
 /* __ Prototypes ________________________________________________________ */
 std::string strip_whitespace(const std::string& arg);
 std::string str(const int& value);
