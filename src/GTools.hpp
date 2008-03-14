097dd38a141928000767cc23e81e7d6dcb8b05ec
knodlseder <>
2008-03-14 00:30:40 +0000

    add fourpi

--- src/GTools.hpp
+++ src/GTools.hpp
@@ -25,6 +25,7 @@
 const double pi      = 3.1415926535897931159979635;
 const double pihalf  = pi/2.0;
 const double twopi   = 2.0*pi;
+const double fourpi  = 4.0*pi;
 const double deg2rad = 0.0174532925199432954743717;
 const double rad2deg = 57.295779513082322864647722;
 const double ln10    = 2.3025850929940459010936138;
