42db527472101d7f8d851cf187f2e34b0310d767
knodlseder <>
2009-10-10 23:13:19 +0000

    do not use memcpy anymore

--- src/fits/GFitsTableFltCol.cpp
+++ src/fits/GFitsTableFltCol.cpp
@@ -380,7 +380,8 @@ void GFitsTableFltCol::copy_members(const GFitsTableFltCol& column)
     if (column.m_data != NULL && m_size > 0) {
         if (m_data != NULL) delete [] m_data;
         m_data = new float[m_size];
-        memcpy(m_data, column.m_data, m_size*sizeof(float));
+        for (int i=0; i < m_size; ++i)
+            m_data[i] = column.m_data[i];
     }
 
     // Copy NULL value
