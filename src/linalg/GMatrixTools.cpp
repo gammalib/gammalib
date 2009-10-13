31131d5e883b4abbbe8f8c2bb1f9d2855554edc9
knodlseder <>
2009-10-13 19:57:14 +0000

    provide symmetry checking during conversion

--- src/linalg/GMatrixTools.cpp
+++ src/linalg/GMatrixTools.cpp
@@ -14,13 +14,17 @@
 /* __ Includes ___________________________________________________________ */
 #include "GMatrixTools.hpp"
 
+/* __ Method name definitions ____________________________________________ */
+#define G_SYM_1                       "GSymMatrix sym_matrix(const GMatrix&)"
+#define G_SYM_2                 "GSymMatrix sym_matrix(const GMatrixSparse&)"
+
 
 /***********************************************************************//**
  * @brief GSymMatrix to GMatrix storage class convertor
  *
  * @param[in] m GSymMatrix to be converted
  ***************************************************************************/
-GMatrix convert(const GSymMatrix& m)
+GMatrix matrix(const GSymMatrix& m)
 {
     // Allocate matrix
     GMatrix result = GMatrix(m.rows(), m.cols());
@@ -41,7 +45,7 @@ GMatrix convert(const GSymMatrix& m)
  *
  * @param[in] m GSparseMatrix to be converted
  ***************************************************************************/
-GMatrix convert(const GSparseMatrix& m)
+GMatrix matrix(const GSparseMatrix& m)
 {
     // Allocate matrix
     GMatrix result = GMatrix(m.rows(), m.cols());
@@ -55,3 +59,98 @@ GMatrix convert(const GSparseMatrix& m)
     // Return
     return result;
 }
+
+
+/***********************************************************************//**
+ * @brief GMatrix to GSymMatrix storage class convertor
+ *
+ * @param[in] m GMatrix to be converted
+ ***************************************************************************/
+GSymMatrix sym_matrix(const GMatrix& m)
+{
+    // Allocate matrix
+    GSymMatrix result = GSymMatrix(m.rows(), m.cols());
+
+    // Fill matrix
+    for (int col = 0; col < m.cols(); ++col) {
+        for (int row = col; row < m.rows(); ++row) {
+            double value_ll = m(row,col);
+            double value_ur = m(col,row);
+            if (value_ll != value_ur)
+                throw GException::matrix_not_symmetric(G_SYM_1, m.rows(), m.cols());
+            result(row, col) = m(row, col);
+        }
+    }
+
+    // Return
+    return result;
+}
+
+
+/***********************************************************************//**
+ * @brief GSparseMatrix to GSymMatrix storage class convertor
+ *
+ * @param[in] m GSparseMatrix to be converted
+ ***************************************************************************/
+GSymMatrix sym_matrix(const GSparseMatrix& m)
+{
+    // Allocate matrix
+    GSymMatrix result = GSymMatrix(m.rows(), m.cols());
+
+    // Fill matrix
+    for (int col = 0; col < m.cols(); ++col) {
+        for (int row = col; row < m.rows(); ++row) {
+            double value_ll = m(row,col);
+            double value_ur = m(col,row);
+            if (value_ll != value_ur)
+                throw GException::matrix_not_symmetric(G_SYM_2, m.rows(), m.cols());
+            result(row, col) = m(row, col);
+        }
+    }
+
+    // Return
+    return result;
+}
+
+
+/***********************************************************************//**
+ * @brief GMatrix to GSparseMatrix storage class convertor
+ *
+ * @param[in] m GMatrix to be converted
+ ***************************************************************************/
+GSparseMatrix sparse_matrix(const GMatrix& m)
+{
+    // Allocate matrix
+    GSparseMatrix result = GSparseMatrix(m.rows(), m.cols());
+
+    // Fill matrix
+    for (int col = 0; col < m.cols(); ++col) {
+        for (int row = 0; row < m.rows(); ++row)
+            result(row, col) = m(row, col);
+    }
+
+    // Return
+    return result;
+}
+
+
+/***********************************************************************//**
+ * @brief GSymMatrix to GSparseMatrix storage class convertor
+ *
+ * @param[in] m GSymMatrix to be converted
+ ***************************************************************************/
+GSparseMatrix sparse_matrix(const GSymMatrix& m)
+{
+    // Allocate matrix
+    GSparseMatrix result = GSparseMatrix(m.rows(), m.cols());
+
+    // Fill matrix
+    for (int col = 0; col < m.cols(); ++col) {
+        for (int row = 0; row < m.rows(); ++row)
+            result(row, col) = m(row, col);
+    }
+
+    // Return
+    return result;
+}
+
