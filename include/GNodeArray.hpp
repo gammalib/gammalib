7fae4791766a77fcac70fa72cc5342756f3f45fd
knodlseder <>
2010-05-26 20:49:32 +0000

    use std::vector for storage and extend class

--- include/GNodeArray.hpp
+++ include/GNodeArray.hpp
@@ -1,7 +1,7 @@
 /***************************************************************************
  *                 GNodeArray.hpp  -  Array of nodes class                 *
  * ----------------------------------------------------------------------- *
- *  copyright            : (C) 2008 by Jurgen Knodlseder                   *
+ *  copyright (C) 2008-2010 by Jurgen Knodlseder                           *
  * ----------------------------------------------------------------------- *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -20,6 +20,8 @@
 #define GNODEARRAY_HPP
 
 /* __ Includes ___________________________________________________________ */
+#include <vector>
+#include <iostream>
 #include "GVector.hpp"
 
 
@@ -28,26 +30,29 @@
  *
  * @brief Interface for the node array class.
  *
- * The node array class collects a number of nodes that may be used to 
- * describe a functional relation. This class may be used to perform a linear
- * interpolation between these nodes. Using the GNodeArray::set_value method, 
- * the indices of the nearest nodes are determined and the weighting factors
- * for linear interpolation are computed. These indices and weighting factors
- * can then be accessed via the methods 
- * GNodeArray::inx_left,
- * GNodeArray::inx_right,
- * GNodeArray::wgt_left, and
- * GNodeArray::wgt_right.
- *
+ * The node array class collects nodes \f$x_i\f$ that may be used to describe
+ * a functional relation \f$y_i=f(x_i)\f$. This class may be used to perform
+ * a linear interpolation between the nodes to determine any value of
+ * \f$y=f(x)\f$.
+ * Nodes are allocated either from a double precision array, a GVector object
+ * or a std::vector using the nodes() method. Alternatively, the node array
+ * may be built on the fly using the append() method.
+ * Interpolation can be either performed using the interpolate() method
+ * or using the set_value(). In the latter case, the node indices and
+ * weighting factors can be recovered using inx_left(), inx_right(),
+ * wgt_left() and wgt_right().
  * If the nodes are equally spaced, interpolation is more rapid.
  ***************************************************************************/
 class GNodeArray {
 
+    // I/O friends
+    friend std::ostream& operator<< (std::ostream& os, const GNodeArray& array);
+
 public:
     // Constructors and destructors
-    GNodeArray();
+    GNodeArray(void);
     GNodeArray(const GNodeArray& array);
-    ~GNodeArray();
+    ~GNodeArray(void);
 
     // Operators
     GNodeArray& operator= (const GNodeArray & array);
@@ -55,6 +60,9 @@ public:
     // Methods
     void   nodes(const int& num, const double* array);
     void   nodes(const GVector& vector);
+    void   nodes(const std::vector<double>& vector);
+    void   append(const double& node);
+    double interpolate(const double& value, const std::vector<double>& vector);
     void   set_value(const double& value);
     int    inx_left(void) { return m_inx_left; }
     int    inx_right(void) { return m_inx_right; }
@@ -69,16 +77,15 @@ private:
     void setup(void);
     
     // Data
-    int     m_nodes;          //!< Number of nodes
-    double* m_node;           //!< Node values
-    double* m_step;           //!< Distance to next node
-    int     m_is_linear;      //!< Nodes form a linear array
-    double  m_linear_slope;   //!< Slope for linear array
-    double  m_linear_offset;  //!< Offset for linear array
-    int     m_inx_left;       //!< Index of left node for linear interpolation
-    int     m_inx_right;      //!< Index of right node for linear interpolation
-    double  m_wgt_left;       //!< Weight for left node for linear interpolation
-    double  m_wgt_right;      //!< Weight for right node for linear interpolation
+    std::vector<double> m_node;          //!< Number of nodes
+    std::vector<double> m_step;          //!< Distance to next node
+    bool                m_is_linear;     //!< Nodes form a linear array
+    double              m_linear_slope;  //!< Slope for linear array
+    double              m_linear_offset; //!< Offset for linear array
+    int                 m_inx_left;      //!< Index of left node for linear interpolation
+    int                 m_inx_right;     //!< Index of right node for linear interpolation
+    double              m_wgt_left;      //!< Weight for left node for linear interpolation
+    double              m_wgt_right;     //!< Weight for right node for linear interpolation
 };
 
 #endif /* GNODEARRAY_HPP */
--- src/support/GNodeArray.cpp
+++ src/support/GNodeArray.cpp
@@ -1,7 +1,7 @@
 /***************************************************************************
  *                 GNodeArray.cpp  -  Array of nodes class                 *
  * ----------------------------------------------------------------------- *
- *  copyright            : (C) 2008 by Jurgen Knodlseder                   *
+ *  copyright (C) 2008-2010 by Jurgen Knodlseder                           *
  * ----------------------------------------------------------------------- *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -9,7 +9,6 @@
  *   the Free Software Foundation; either version 2 of the License, or     *
  *   (at your option) any later version.                                   *
  *                                                                         *
- * ----------------------------------------------------------------------- *
  ***************************************************************************/
 /**
  * @file GNodeArray.cpp
@@ -18,12 +17,16 @@
  */
 
 /* __ Includes ___________________________________________________________ */
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
 #include <iostream>
 #include <math.h>
 #include "GException.hpp"
 #include "GNodeArray.hpp"
 
 /* __ Method name definitions ____________________________________________ */
+#define G_INTERPOLATE "GNodeArray::interpolate(std::vector<double>&, double&"
 
 /* __ Macros _____________________________________________________________ */
 
@@ -36,14 +39,14 @@
 
 /*==========================================================================
  =                                                                         =
- =                    GNodeArray constructors/destructors                  =
+ =                        Constructors/destructors                         =
  =                                                                         =
  ==========================================================================*/
 
 /***********************************************************************//**
  * @brief Constructor
  ***************************************************************************/
-GNodeArray::GNodeArray()
+GNodeArray::GNodeArray(void)
 {
     // Initialise class members for clean destruction
     init_members();
@@ -56,7 +59,7 @@ GNodeArray::GNodeArray()
 /***********************************************************************//**
  * @brief Copy constructor
  *
- * @param array Array to be copied
+ * @param[in] array Instance from which object should be constructed.
  ***************************************************************************/
 GNodeArray::GNodeArray(const GNodeArray& array)
 {
@@ -74,7 +77,7 @@ GNodeArray::GNodeArray(const GNodeArray& array)
 /***********************************************************************//**
  * @brief Destructor
  ***************************************************************************/
-GNodeArray::~GNodeArray()
+GNodeArray::~GNodeArray(void)
 {
     // Free members
     free_members();
@@ -86,14 +89,14 @@ GNodeArray::~GNodeArray()
 
 /*==========================================================================
  =                                                                         =
- =                          GNodeArray operators                           =
+ =                               Operators                                 =
  =                                                                         =
  ==========================================================================*/
 
 /***********************************************************************//**
  * @brief Assignment operator
  *
- * @param array Array to be assigned
+ * @param[in] array Instance that should be assigned.
  ***************************************************************************/
 GNodeArray& GNodeArray::operator= (const GNodeArray& array)
 {
@@ -118,7 +121,7 @@ GNodeArray& GNodeArray::operator= (const GNodeArray& array)
 
 /*==========================================================================
  =                                                                         =
- =                         GNodeArray public methods                       =
+ =                             Public methods                              =
  =                                                                         =
  ==========================================================================*/
 
@@ -126,7 +129,7 @@ GNodeArray& GNodeArray::operator= (const GNodeArray& array)
  * @brief Set node array
  *
  * @param[in] num Number of nodes
- * @param[in] array Values of node array.
+ * @param[in] array Node values \f$x_i\f$.
  ***************************************************************************/
 void GNodeArray::nodes(const int& num, const double* array)
 {
@@ -136,20 +139,11 @@ void GNodeArray::nodes(const int& num, const double* array)
     // Initialise members
     init_members();
     
-    // Set number of nodes
-    m_nodes = num;
-        
-    // Allocate memory
-    if (m_nodes > 0) {
-        m_node = new double[m_nodes];
-        m_step = new double[m_nodes];
-    }
-    
-    // Copy node data
-    for (int i = 0; i < m_nodes; ++i)
-        m_node[i] = array[i];
+    // Set node values
+    for (int i = 0; i < num; ++i)
+        m_node.push_back(array[i]);
 
-    // Setup
+    // Setup node distances and linear array handling
     setup();
 
     // Return
@@ -170,24 +164,89 @@ void GNodeArray::nodes(const GVector& vector)
     // Initialise members
     init_members();
     
-    // Set number of nodes
-    m_nodes = vector.size();
-        
-    // Allocate memory
-    if (m_nodes > 0) {
-        m_node = new double[m_nodes];
-        m_step = new double[m_nodes];
-    }
-    
-    // Copy node data
-    for (int i = 0; i < m_nodes; ++i)
-        m_node[i] = vector(i);
+    // Set node values
+    for (int i = 0; i < vector.size(); ++i)
+        m_node.push_back(vector(i));
 
-    // Setup
+    // Setup node distances and linear array handling
     setup();
 
     // Return
     return;
+}
+
+
+/***********************************************************************//**
+ * @brief Set node array from vector
+ *
+ * @param[in] vector Vector from which node array will be built.
+ ***************************************************************************/
+void GNodeArray::nodes(const std::vector<double>& vector)
+{
+    // Free any existing memory
+    free_members();
+    
+    // Initialise members
+    init_members();
+    
+    // Set node values
+    m_node = vector;
+
+    // Setup node distances and linear array handling
+    setup();
+
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Append one node to array.
+ *
+ * @param[in] node Node to be appended to array.
+ ***************************************************************************/
+void GNodeArray::append(const double& node)
+{
+    // Add node
+    m_node.push_back(node);
+    
+    // Setup node distances and linear array handling
+    setup();
+
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Interpolate value.
+ *
+ * @param[in] value Value \f$x\f$ at which interpolation should be done.
+ * @param[in] vector Vector \f$y_i\f$ that should be interpolated.
+ *
+ * @exception GException::vector_mismatch
+ *            Size of node vector does not match the size of vector argument.
+ *
+ * This method performs a linear interpolation of values \f$y_i\f$. The
+ * corresponding values \f$x_i\f$ are stored in the node array.
+ ***************************************************************************/
+double GNodeArray::interpolate(const double& value,
+                               const std::vector<double>& vector)
+{
+    // Throw exception if vectors have not the same size
+    if (m_node.size() != vector.size())
+        throw GException::vector_mismatch(G_INTERPOLATE, m_node.size(),
+                                          vector.size());
+    
+    // Set interpolation value
+    set_value(value);
+
+    // Interpolate
+    double y = vector[inx_left()]  * wgt_left() +
+               vector[inx_right()] * wgt_right();
+
+    // Return
+    return y;
 }
 
 
@@ -204,49 +263,57 @@ void GNodeArray::nodes(const GVector& vector)
  ***************************************************************************/
 void GNodeArray::set_value(const double& value)
 {
-    // If array is linear then get left index from analytic formula
-    if (m_is_linear) {
+    // Get number of nodes
+    int nodes = m_node.size();
+    
+    // Continue only if we have at least 2 nodes
+    if (nodes > 1) {
+    
+        // If array is linear then get left index from analytic formula
+        if (m_is_linear) {
 
-        // Set left index
-        m_inx_left = int(m_linear_slope * value + m_linear_offset);
+            // Set left index
+            m_inx_left = int(m_linear_slope * value + m_linear_offset);
 
-        // Keep index in valid range
-        if (m_inx_left < 0)               m_inx_left = 0;
-        else if (m_inx_left >= m_nodes-1) m_inx_left = m_nodes - 2;
+            // Keep index in valid range
+            if (m_inx_left < 0)             m_inx_left = 0;
+            else if (m_inx_left >= nodes-1) m_inx_left = nodes - 2;
 
-    } // endif: array is linear
+        } // endif: array is linear
 
-    // ... otherwise search the relevant indices by bisection
-    else {
-        // Set left index if value is before first node
-        if (value < m_node[0])
-            m_inx_left = 0;
-
-        // Set left index if value is after last node
-        else if (value >  m_node[m_nodes-1])
-            m_inx_left = m_nodes - 2;
-
-        // Set left index by bisection
+        // ... otherwise search the relevant indices by bisection
         else {
-            int low  = 0;
-            int high = m_nodes - 1;
-            while ((high - low) > 1) {
-                int mid = (low+high) / 2;
-                if (m_node[mid] > value)
-                    high = mid;
-                else if (m_node[mid] <= value)
-                    low = mid;
-            }
-            m_inx_left = low;
-        } // endelse: did bisection
-    }
+            // Set left index if value is before first node
+            if (value < m_node[0])
+                m_inx_left = 0;
 
-    // Set right index
-    m_inx_right = m_inx_left + 1;
+            // Set left index if value is after last node
+            else if (value >  m_node[nodes-1])
+                m_inx_left = nodes - 2;
 
-    // Set weighting factors
-    m_wgt_right = (value - m_node[m_inx_left]) / m_step[m_inx_left];
-    m_wgt_left  = 1.0 - m_wgt_right;
+            // Set left index by bisection
+            else {
+                int low  = 0;
+                int high = nodes - 1;
+                while ((high - low) > 1) {
+                    int mid = (low+high) / 2;
+                    if (m_node[mid] > value)
+                        high = mid;
+                    else if (m_node[mid] <= value)
+                        low = mid;
+                }
+                m_inx_left = low;
+            } // endelse: did bisection
+        }
+
+        // Set right index
+        m_inx_right = m_inx_left + 1;
+
+        // Set weighting factors
+        m_wgt_right = (value - m_node[m_inx_left]) / m_step[m_inx_left];
+        m_wgt_left  = 1.0 - m_wgt_right;
+    
+    } // endif: there were at least 2 nodes
 
     // Return
     return;
@@ -265,9 +332,11 @@ void GNodeArray::set_value(const double& value)
 void GNodeArray::init_members(void)
 {
     // Initialise members
-    m_nodes         = 0;
-    m_node          = NULL;
-    m_step          = NULL;
+//    m_nodes         = 0;
+//    m_node          = NULL;
+//    m_step          = NULL;
+    m_node.clear();
+    m_step.clear();
     m_is_linear     = 0;
     m_linear_slope  = 0.0;
     m_linear_offset = 0.0;
@@ -280,24 +349,27 @@ void GNodeArray::init_members(void)
 /***********************************************************************//**
  * @brief Copy class members
  *
- * @param[in] array Array to be copied
+ * @param[in] array Node array to be copied.
  ***************************************************************************/
 void GNodeArray::copy_members(const GNodeArray& array)
 {
     // Copy number of bins
-    m_nodes         = array.m_nodes;
+    //m_nodes         = array.m_nodes;
+    m_node          = array.m_node;
+    m_step          = array.m_step;
     m_is_linear     = array.m_is_linear;
     m_linear_slope  = array.m_linear_slope;
     m_linear_offset = array.m_linear_offset;
     
     // Copy nodes
+/*
     if (m_nodes > 0) {
         m_node = new double[m_nodes];
         m_step = new double[m_nodes];
         memcpy(m_node, array.m_node, m_nodes*sizeof(double));
         memcpy(m_step, array.m_step, m_nodes*sizeof(double));
     }
-
+*/
     // Return
     return;
 }
@@ -309,12 +381,12 @@ void GNodeArray::copy_members(const GNodeArray& array)
 void GNodeArray::free_members(void)
 {
     // Free memory
-    if (m_node != NULL) delete [] m_node;
-    if (m_step != NULL) delete [] m_step;
+//    if (m_node != NULL) delete [] m_node;
+//    if (m_step != NULL) delete [] m_step;
 
     // Signal that memory is free
-    m_node = NULL;
-    m_step = NULL;
+//    m_node = NULL;
+//    m_step = NULL;
 
     // Return
     return;
@@ -326,24 +398,74 @@ void GNodeArray::free_members(void)
  ***************************************************************************/
 void GNodeArray::setup(void)
 {
-    // Setup distance array between subsequent nodes
-    for (int i = 0; i < m_nodes-1; ++i)
-        m_step[i] = m_node[i+1] - m_node[i];
-
-    // Evaluate linear slope and offset
-    m_linear_slope  = double(m_nodes-1) / (m_node[m_nodes-1] - m_node[0]);
-    m_linear_offset = -m_linear_slope * m_node[0];
+    // Reset distance vector
+    m_step.clear();
     
-    // Check if nodes form a linear array
-    m_is_linear = 1;
-    for (int i = 0; i < m_nodes-1; ++i) {
-        double eps = m_linear_slope * m_node[i] + m_linear_offset - double(i);
-        if (fabs(eps) > 1.0e-6) {
-            m_is_linear = 0;
-            break;
+    // Get number of nodes
+    int nodes = m_node.size();
+    
+    // Continue only if we have nodes at least 2 nodes
+    if (nodes > 1) {
+    
+        // Setup distance array between subsequent nodes
+        for (int i = 0; i < nodes-1; ++i)
+            m_step.push_back(m_node[i+1] - m_node[i]);
+
+        // Evaluate linear slope and offset
+        m_linear_slope  = double(nodes-1) / (m_node[nodes-1] - m_node[0]);
+        m_linear_offset = -m_linear_slope * m_node[0];
+    
+        // Check if nodes form a linear array
+        m_is_linear = true;
+        for (int i = 0; i < nodes-1; ++i) {
+            double eps = m_linear_slope * m_node[i] + m_linear_offset - double(i);
+            if (fabs(eps) > 1.0e-6) {
+                m_is_linear = false;
+                break;
+            }
         }
-    }
+        
+    } // endif: there were at least 2 nodes
 
     // Return
     return;
+}
+
+
+/*==========================================================================
+ =                                                                         =
+ =                                 Friends                                 =
+ =                                                                         =
+ ==========================================================================*/
+
+/***********************************************************************//**
+ * @brief Put object in output stream
+ *
+ * @param[in] os Output stream into which the model will be dumped
+ * @param[in] array Node array to be dumped
+ ***************************************************************************/
+std::ostream& operator<< (std::ostream& os,const GNodeArray& array)
+{
+    // Put object in stream
+    os << "=== GNodeArray ===" << std::endl;
+    os << " Number of nodes in array ..: " << array.m_node.size() << std::endl;
+    if (array.m_is_linear) {
+        os << " Array type ................: linear" << std::endl;
+        os << " Linear slope ..............: " << array.m_linear_slope
+           << std::endl;
+        os << " Linear offset .............: " << array.m_linear_offset
+           << std::endl;
+    }
+    else
+        os << " Array type ................: nonlinear" << std::endl;
+    os << " Indices and weights .......: (" 
+       << array.m_inx_left << "," 
+       << array.m_inx_right << ")=("
+       << array.m_wgt_left << ","
+       << array.m_wgt_right << ")" << std::endl;
+    for (int i = 0; i < array.m_node.size(); ++i)
+        os << " " << array.m_node.at(i);
+
+    // Return output stream
+    return os;
 }
