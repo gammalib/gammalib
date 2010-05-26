c3fb79bc854c6ca31c87702e570b153b5778a897
knodlseder <>
2010-05-26 07:24:28 +0000

    initial version

---
+++ src/cta/GCTAException.cpp
@@ -0,0 +1,50 @@
+/***************************************************************************
+ *                 GCTAException.cpp  - CTA exception handler              *
+ * ----------------------------------------------------------------------- *
+ *  copyright (C) 2010 by Jurgen Knodlseder                                *
+ * ----------------------------------------------------------------------- *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+/**
+ * @file GCTAException.cpp
+ * @brief CTA exception handler interface implementation.
+ * @author J. Knodlseder
+ */
+
+/* __ Includes ___________________________________________________________ */
+#include "GCTAException.hpp"
+#include "GTools.hpp"
+
+
+/***********************************************************************//**
+ * @brief Exception message.
+ ***************************************************************************/
+const char* GCTAExceptionHandler::what() const throw()
+{
+    std::string message = "*** ERROR in " + m_origin + ": " + m_message;
+    return message.c_str();
+}
+
+
+/***********************************************************************//**
+ * @brief Error while opening file.
+ *
+ * @param[in] origin Name of method that has thrown the exception.
+ * @param[in] filename Name of file that could not be opened.
+ * @param[in] message Optional error message.
+ ***************************************************************************/
+GCTAException::file_open_error::file_open_error(std::string origin,
+                                                std::string filename,
+                                                std::string message)
+{
+    m_origin  = origin;
+    m_message = "Unable to open file '"+filename+"'. "+message;
+    return;
+}
+
+
