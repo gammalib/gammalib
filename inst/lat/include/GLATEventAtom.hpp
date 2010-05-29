a7bf17a1ab2bbbf7b25af3025111888f2703fc35
knodlseder <>
2010-05-29 00:31:54 +0000

    add LAT attributes

--- inst/lat/include/GLATEventAtom.hpp
+++ inst/lat/include/GLATEventAtom.hpp
@@ -1,7 +1,7 @@
 /***************************************************************************
  *                GLATEventAtom.hpp  -  LAT event atom class               *
  * ----------------------------------------------------------------------- *
- *  copyright (C) 2009 by Jurgen Knodlseder                                *
+ *  copyright (C) 2009-2010 by Jurgen Knodlseder                           *
  * ----------------------------------------------------------------------- *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -20,9 +20,15 @@
 #define GLATEVENTATOM_HPP
 
 /* __ Includes ___________________________________________________________ */
+#include <ostream>
 #include "GEventAtom.hpp"
 #include "GModels.hpp"
 #include "GVector.hpp"
+#include "GEnergy.hpp"
+#include "GTime.hpp"
+#include "GLATInstDir.hpp"
+#include "GLATPointing.hpp"
+#include "GLATResponse.hpp"
 
 
 /***********************************************************************//**
@@ -35,18 +41,24 @@ class GLATEventAtom : public GEventAtom {
     // Friend classes
     friend class GLATEventList;
 
+    // I/O friends
+    friend std::ostream& operator<< (std::ostream& os, const GLATEventAtom& atom);
+
 public:
     // Constructors and destructors
-    GLATEventAtom();
+    GLATEventAtom(void);
     GLATEventAtom(const GLATEventAtom& atom);
-    virtual ~GLATEventAtom();
+    virtual ~GLATEventAtom(void);
 
     // Operators
     GLATEventAtom& operator= (const GLATEventAtom& atom);
 
     // Methods
-    double model(GModels& models);
-    double model(GModels& models, GVector* gradient);
+    double              model(GModels& models) const;
+    double              model(GModels& models, GVector* gradient) const;
+    const GLATInstDir*  dir(void) const { return &m_dir; }
+    const GLATPointing* pnt(void) const { return m_pnt; }
+    const GLATResponse* rsp(void) const { return m_rsp; }
     
 protected:
     // Protected methods
@@ -54,6 +66,11 @@ protected:
     void           copy_members(const GLATEventAtom& atom);
     void           free_members(void);
     GLATEventAtom* clone(void) const;
+
+    // LAT attributes
+    GLATInstDir   m_dir;            //!< Event direction
+    GLATPointing* m_pnt;            //!< Pointer to CTA pointing
+    GLATResponse* m_rsp;            //!< Pointer to CTA instrument response function
 
     // LAT data format attributes
     float   m_theta;                //!< Zenith angle in instrument system
