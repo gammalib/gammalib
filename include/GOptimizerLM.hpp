00f76742b3f0fe4b8ded84fdacb2495b62696e0e
knodlseder <>
2010-06-07 21:08:01 +0000

    add configuration handling methods

--- include/GOptimizerLM.hpp
+++ include/GOptimizerLM.hpp
@@ -1,7 +1,7 @@
 /***************************************************************************
  *            GOptimizerLM.hpp  -  Levenberg Marquardt optimizer           *
  * ----------------------------------------------------------------------- *
- *  copyright (C) 2009 by Jurgen Knodlseder                                *
+ *  copyright (C) 2009-2010 by Jurgen Knodlseder                           *
  * ----------------------------------------------------------------------- *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
@@ -44,14 +44,32 @@ class GOptimizerLM : public GOptimizer {
 public:
 
     // Constructors and destructors
-    GOptimizerLM();
+    GOptimizerLM(void);
     GOptimizerLM(const GOptimizerLM& opt);
-    virtual ~GOptimizerLM();
+    virtual ~GOptimizerLM(void);
 
     // Operators
     GOptimizerLM&   operator= (const GOptimizerLM& opt);
     GOptimizerPars& operator() (GOptimizerFunction& fct, GOptimizerPars& p);
     GModels&        operator() (GOptimizerFunction& fct, GModels& m);
+    
+    // Methods
+    void   max_iter(const int& n) { m_max_iter=n; return; }
+    void   max_stalls(const int& n) { m_max_stall=n; return; }
+    void   lambda_start(const double& val) { m_lambda_start=val; return; }
+    void   lambda_inc(const double& val) { m_lambda_inc=val; return; }
+    void   lambda_dec(const double& val) { m_lambda_dec=val; return; }
+    void   eps(const double& eps) { m_eps=eps; return; }
+    int    max_iter(void) const { return m_max_iter; }
+    int    max_stalls(void) const { return m_max_stall; }
+    int    status(void) const { return m_status; }
+    int    iter(void) const { return m_iter; }
+    double lambda_start(void) const { return m_lambda_start; }
+    double lambda_inc(void) const { return m_lambda_inc; }
+    double lambda_dec(void) const { return m_lambda_dec; }
+    double lambda(void) const { return m_lambda; }
+    double eps(void) const { return m_eps; }
+    double value(void) const { return m_value; }
 
 protected:
     // Protected methods
