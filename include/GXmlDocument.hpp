6db419353acd850efe2f1d0a6182c9108cd7673e
knodlseder <>
2010-10-18 19:28:31 +0000

    add attributes

--- include/GXmlDocument.hpp
+++ include/GXmlDocument.hpp
@@ -21,13 +21,18 @@
 
 /* __ Includes ___________________________________________________________ */
 #include <iostream>
+#include <string>
 #include "GXmlNode.hpp"
+#include "GXmlAttribute.hpp"
 
 
 /***********************************************************************//**
  * @class GXmlDocument
  *
  * @brief XML document node class interface defintion.
+ *
+ * This class implements the root node of an XML document. It contains the
+ * three special attributes 'version', 'encoding', and 'standalone'.
  ***************************************************************************/
 class GXmlDocument : public GXmlNode {
 
@@ -41,9 +46,15 @@ public:
     GXmlDocument& operator= (const GXmlDocument& node);
 
     // Methods
-    void     clear(void);
-    void     print(std::ostream& os, int indent = 0) const;
-    NodeType type(void) const { return NT_DOCUMENT; }
+    void        clear(void);
+    void        print(std::ostream& os, int indent = 0) const;
+    NodeType    type(void) const { return NT_DOCUMENT; }
+    std::string version(void) const { return m_version.value(); }
+    std::string encoding(void) const { return m_encoding.value(); }
+    std::string standalone(void) const { return m_standalone.value(); }
+    void        version(const std::string& version) { m_version.value(version); }
+    void        encoding(const std::string& encoding) { m_encoding.value(encoding); }
+    void        standalone(const std::string& standalone) { m_standalone.value(standalone); }
 
 protected:
     // Protected methods
@@ -53,6 +64,9 @@ protected:
     GXmlDocument* GXmlDocument::clone(void) const;
 
     // Protected data members
+    GXmlAttribute m_version;      //!< XML version ("1.0", "1.1")
+    GXmlAttribute m_encoding;     //!< Encoding (e.g. "UTF-8")
+    GXmlAttribute m_standalone;   //!< Standalone ("yes", "no") 
 };
 
 #endif /* GXMLDOCUMENT_HPP */
