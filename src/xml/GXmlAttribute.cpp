73b174fa51094c3db334f6850dd666f3d7647558
knodlseder <>
2010-10-18 19:37:17 +0000

    initial version

---
+++ src/xml/GXmlAttribute.cpp
@@ -0,0 +1,236 @@
+/***************************************************************************
+ *       GXmlAttribute.cpp - XML attribute node class implementation       *
+ * ----------------------------------------------------------------------- *
+ *  copyright (C) 2010 by Jurgen Knodlseder                                *
+ * ----------------------------------------------------------------------- *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+/**
+ * @file GXmlAttribute.cpp
+ * @brief XML attribute node class implementation
+ * @author J. Knodlseder
+ */
+
+/* __ Includes ___________________________________________________________ */
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+#include <iostream>
+#include "GXmlAttribute.hpp"
+
+/* __ Method name definitions ____________________________________________ */
+
+/* __ Macros _____________________________________________________________ */
+
+/* __ Coding definitions _________________________________________________ */
+
+/* __ Debug definitions __________________________________________________ */
+
+
+/*==========================================================================
+ =                                                                         =
+ =                         Constructors/destructors                        =
+ =                                                                         =
+ ==========================================================================*/
+
+/***********************************************************************//**
+ * @brief Void constructor
+ ***************************************************************************/
+GXmlAttribute::GXmlAttribute(void)
+{
+    // Initialise private members for clean destruction
+    init_members();
+
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Copy constructor
+ *
+ * @param[in] attr Object from which the instance should be built.
+ ***************************************************************************/
+GXmlAttribute::GXmlAttribute(const GXmlAttribute& attr)
+{
+    // Initialise private members for clean destruction
+    init_members();
+
+    // Copy members
+    copy_members(attr);
+
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Name-Value pair constructor
+ *
+ * @param[in] name Name for instance building.
+ * @param[in] value Value for instance building.
+ ***************************************************************************/
+GXmlAttribute::GXmlAttribute(const std::string& name, const std::string& value)
+{
+    // Initialise private members for clean destruction
+    init_members();
+
+    // Set attribute
+    m_name  = name;
+    m_value = value;
+
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Destructor
+ ***************************************************************************/
+GXmlAttribute::~GXmlAttribute(void)
+{
+    // Free members
+    free_members();
+
+    // Return
+    return;
+}
+
+
+/*==========================================================================
+ =                                                                         =
+ =                                Operators                                =
+ =                                                                         =
+ ==========================================================================*/
+
+/***********************************************************************//**
+ * @brief Assignment operator
+ *
+ * @param[in] attr Object which should be assigned.
+ ***************************************************************************/
+GXmlAttribute& GXmlAttribute::operator= (const GXmlAttribute& attr)
+{
+    // Execute only if object is not identical
+    if (this != &attr) {
+
+        // Free members
+        free_members();
+
+        // Initialise private members for clean destruction
+        init_members();
+
+        // Copy members
+        copy_members(attr);
+
+    } // endif: object was not identical
+
+    // Return
+    return *this;
+}
+
+
+/*==========================================================================
+ =                                                                         =
+ =                             Public methods                              =
+ =                                                                         =
+ ==========================================================================*/
+ 
+ /***********************************************************************//**
+ * @brief Clear object.
+ *
+ * This method properly resets the object to an initial state.
+ ***************************************************************************/
+void GXmlAttribute::clear(void)
+{
+    // Free class members (base and derived classes, derived class first)
+    free_members();
+
+    // Initialise members
+    init_members();
+
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Print node in output stream
+ *
+ * @param[in] os Output stream into which the node will be printed.
+ ***************************************************************************/
+void GXmlAttribute::print(std::ostream& os) const
+{
+    // Put attribute in output stream
+    os << " " << m_name << "=" << m_value;
+
+    // Return
+    return;
+}
+
+
+/*==========================================================================
+ =                                                                         =
+ =                             Private methods                             =
+ =                                                                         =
+ ==========================================================================*/
+
+/***********************************************************************//**
+ * @brief Initialise class members
+ ***************************************************************************/
+void GXmlAttribute::init_members(void)
+{
+    // Initialise members
+    m_name.clear();
+    m_value.clear();
+
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Copy class members
+ *
+ * @param[in] node Object from which members which should be copied.
+ ***************************************************************************/
+void GXmlAttribute::copy_members(const GXmlAttribute& attr)
+{
+    // Copy attributes
+    m_name  = attr.m_name;
+    m_value = attr.m_value;
+
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Delete class members
+ ***************************************************************************/
+void GXmlAttribute::free_members(void)
+{
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Clone class
+***************************************************************************/
+GXmlAttribute* GXmlAttribute::clone(void) const
+{
+    return new GXmlAttribute(*this);
+}
+
+
+/*==========================================================================
+ =                                                                         =
+ =                                 Friends                                 =
+ =                                                                         =
+ ==========================================================================*/
+
---
+++ src/xml/GXmlComment.cpp
@@ -0,0 +1,273 @@
+/***************************************************************************
+ *         GXmlComment.cpp - XML comment node class implementation         *
+ * ----------------------------------------------------------------------- *
+ *  copyright (C) 2010 by Jurgen Knodlseder                                *
+ * ----------------------------------------------------------------------- *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+/**
+ * @file GXmlComment.cpp
+ * @brief XML comment node class implementation
+ * @author J. Knodlseder
+ */
+
+/* __ Includes ___________________________________________________________ */
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+#include <iostream>
+#include "GException.hpp"
+#include "GXmlComment.hpp"
+
+/* __ Method name definitions ____________________________________________ */
+#define G_PARSE                            "GXmlComment::parse(std::string&)"
+
+/* __ Macros _____________________________________________________________ */
+
+/* __ Coding definitions _________________________________________________ */
+
+/* __ Debug definitions __________________________________________________ */
+
+
+/*==========================================================================
+ =                                                                         =
+ =                         Constructors/destructors                        =
+ =                                                                         =
+ ==========================================================================*/
+
+/***********************************************************************//**
+ * @brief Void constructor
+ ***************************************************************************/
+GXmlComment::GXmlComment(void) : GXmlNode()
+{
+    // Initialise private members for clean destruction
+    init_members();
+
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Copy constructor
+ *
+ * @param[in] node Object from which the instance should be built.
+ ***************************************************************************/
+GXmlComment::GXmlComment(const GXmlComment& node) : GXmlNode(node)
+{
+    // Initialise private members for clean destruction
+    init_members();
+
+    // Copy members
+    copy_members(node);
+
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Segment constructor
+ *
+ * @param[in] text Text for instance building.
+ ***************************************************************************/
+GXmlComment::GXmlComment(const std::string& segment) : GXmlNode()
+{
+    // Initialise private members for clean destruction
+    init_members();
+
+    // Parse segment
+    parse(segment);
+
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Destructor
+ ***************************************************************************/
+GXmlComment::~GXmlComment(void)
+{
+    // Free members
+    free_members();
+
+    // Return
+    return;
+}
+
+
+/*==========================================================================
+ =                                                                         =
+ =                                Operators                                =
+ =                                                                         =
+ ==========================================================================*/
+
+/***********************************************************************//**
+ * @brief Assignment operator
+ *
+ * @param[in] node Object which should be assigned.
+ ***************************************************************************/
+GXmlComment& GXmlComment::operator= (const GXmlComment& node)
+{
+    // Execute only if object is not identical
+    if (this != &node) {
+
+        // Copy base class members
+        this->GXmlNode::operator=(node);
+
+        // Free members
+        free_members();
+
+        // Initialise private members for clean destruction
+        init_members();
+
+        // Copy members
+        copy_members(node);
+
+    } // endif: object was not identical
+
+    // Return
+    return *this;
+}
+
+
+/*==========================================================================
+ =                                                                         =
+ =                             Public methods                              =
+ =                                                                         =
+ ==========================================================================*/
+ 
+ /***********************************************************************//**
+ * @brief Clear object.
+ *
+ * This method properly resets the object to an initial state.
+ ***************************************************************************/
+void GXmlComment::clear(void)
+{
+    // Free class members (base and derived classes, derived class first)
+    free_members();
+    this->GXmlNode::free_members();
+
+    // Initialise members
+    this->GXmlNode::init_members();
+    init_members();
+
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Print node in output stream
+ *
+ * @param[in] os Output stream into which the node will be printed.
+ ***************************************************************************/
+void GXmlComment::print(std::ostream& os, int indent) const
+{
+    // Put element name in output stream
+    for (int k = 0; k < indent; ++k)
+        os << " ";
+    os << "GXmlComment::" << m_comment;
+
+    // Return
+    return;
+}
+
+
+/*==========================================================================
+ =                                                                         =
+ =                             Private methods                             =
+ =                                                                         =
+ ==========================================================================*/
+
+/***********************************************************************//**
+ * @brief Initialise class members
+ ***************************************************************************/
+void GXmlComment::init_members(void)
+{
+    // Initialise members
+    m_comment.clear();
+
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Copy class members
+ *
+ * @param[in] node Object from which members which should be copied.
+ ***************************************************************************/
+void GXmlComment::copy_members(const GXmlComment& node)
+{
+    // Copy attributes
+    m_comment = node.m_comment;
+
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Delete class members
+ ***************************************************************************/
+void GXmlComment::free_members(void)
+{
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Clone class
+***************************************************************************/
+GXmlComment* GXmlComment::clone(void) const
+{
+    return new GXmlComment(*this);
+}
+
+
+/***********************************************************************//**
+ * @brief Parse comment segment string
+ *
+ * @param[in] segement Segment string.
+ *
+ * @exception GException::xml_syntax_error
+ *            XML syntax error.
+ *
+ * Parse the segment string.
+ ***************************************************************************/
+void GXmlComment::parse(const std::string& segment)
+{
+    // Get length of segment
+    int n = segment.length();
+
+    // Check on existence of brackets
+    if (n < 7 || (segment.compare(0,4,"<!--") != 0) &&
+                 (segment.compare(n-3,3,"-->") != 0))
+        throw GException::xml_syntax_error(G_PARSE, segment,
+                          "missing or invalid comment brackets");
+
+    // Set comment
+    if (n > 7)
+        m_comment = segment.substr(4, n-7);
+    else
+        m_comment.clear();
+
+    // Return
+    return;
+}
+
+
+/*==========================================================================
+ =                                                                         =
+ =                                 Friends                                 =
+ =                                                                         =
+ ==========================================================================*/
+
