18c1194e7d762eae0686a0b8b3e01c55e7beb35a
knodlseder <>
2010-11-12 20:03:59 +0000

    first working implementation of logger

--- include/GLog.hpp
+++ include/GLog.hpp
@@ -20,40 +20,94 @@
 #define GLOG_HPP
 
 /* __ Includes ___________________________________________________________ */
-#include <time.h>
-#include <vector>
 #include <string>
-#include <iostream>
+
+/* __ Forward declarations _______________________________________________ */
 
 
 /***********************************************************************//**
  * @class GLog
  *
  * @brief Information logger interface defintion.
+ *
+ * This class implements an interface for logging of text messages to the
+ * standard output and/or error streams and into an ASCII log file. It
+ * provides a C++ style interface (allowing to stream messages using code
+ * such as
+ * log << "This is a message";
+ * and a C style interface that allows making use of C style text formating
+ * using code such as
+ * log("These are %d messages with %d items.", n, m);
+ * The logger interface implements an internal character buffer that is
+ * flushed once a maximum size parameter is exceeded. The buffer limit can
+ * be modified using the max_size() method; by default it is set to 8192
+ * characters. Note that the buffer is flushed each time a carriage return
+ * occurs, irrespectively of the buffer size.
+ * The logger allows prepending of the current date and the task name. Note
+ * that the current date is the one that applies when the buffer is actually
+ * flushed, hence when a good precision in the date is required the maximum
+ * buffer length should be set to 0 (i.e. the buffer is flushed after each
+ * logging).
  ***************************************************************************/
 class GLog {
-
-    // I/O friends
-    friend std::ostream& operator<< (std::ostream& os, const GLog& log);
 
 public:
     // Constructors and destructors
     GLog(void);
+    GLog(const std::string& filename, bool clobber = false);
     GLog(const GLog& log);
     ~GLog(void);
  
     // Operators
     GLog& operator= (const GLog& log);
+    void  operator()(const char *msgFormat, ...);
+    GLog& operator<<(const std::string& value);
+    GLog& operator<<(const bool& value);
+    GLog& operator<<(const int& value);
+    GLog& operator<<(const double& value);
+    GLog& operator<<(const char& value);
+    GLog& operator<<(std::ostream& (*fn)(std::ostream&));
 
     // Methods
-  
+    void        clear(void);
+    int         size(void) const;
+    void        open(const std::string& filename, bool clobber = false);
+    void        close(void);
+    void        date(bool flag);
+    void        cout(bool flag);
+    void        cerr(bool flag);
+    void        name(const std::string& name);
+    void        max_size(int size);
+    void        indent(int indent);
+    void        header0(const std::string& arg) { header(arg, 0); }
+    void        header1(const std::string& arg) { header(arg, 1); }
+    void        header2(const std::string& arg) { header(arg, 2); }
+    bool        date(void) const { return m_use_date; }
+    bool        cout(void) const { return m_stdout; }
+    bool        cerr(void) const { return m_stderr; }
+    std::string name(void) const { return m_name; }
+    int         max_size(void) const { return m_max_length; }
+    int         indent(void) const { return m_indent; }
+
 protected:
     // Protected methods
-    void init_members(void);
-    void copy_members(const GLog& log);
-    void free_members(void);
+    void        init_members(void);
+    void        copy_members(const GLog& log);
+    void        free_members(void);
+    void        flush(bool force = false);
+    void        header(const std::string& arg, int level);
+    std::string strdate(void);
 
     // Protected data members
+    int         m_max_length;    //!< Maximum buffer length
+    int         m_indent;        //!< Indentation of text
+    bool        m_stdout;        //!< Dump in standard output
+    bool        m_stderr;        //!< Dump in standard error
+    bool        m_use_date;      //!< Dump date in prefix
+    bool        m_newline;       //!< Signal beginning of new line
+    FILE*       m_file;          //!< Log file pointer
+    std::string m_name;          //!< Name for prefix
+    std::string m_buffer;        //!< Output string buffer
 };
 
 #endif /* GLOG_HPP */
--- src/app/GLog.cpp
+++ src/app/GLog.cpp
@@ -17,7 +17,16 @@
  */
 
 /* __ Includes ___________________________________________________________ */
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <time.h>
+#include <stdarg.h>     // for "va_list" type
 #include "GLog.hpp"
+#include "GTools.hpp"
 
 /* __ Method name definitions ____________________________________________ */
 
@@ -41,6 +50,27 @@ GLog::GLog(void)
 {
     // Initialise private members for clean destruction
     init_members();
+
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Log file constructor
+ *
+ * @param[in] filename Name of log file.
+ * @param[in] clobber If true overwrite any existing file, otherwise append.
+ *
+ * Construct a logger with an open log file.
+ ***************************************************************************/
+GLog::GLog(const std::string& filename, bool clobber)
+{
+    // Initialise private members for clean destruction
+    init_members();
+
+    // Open file
+    open(filename, clobber);
 
     // Return
     return;
@@ -110,11 +140,363 @@ GLog& GLog::operator= (const GLog& log)
 }
 
 
+/**************************************************************************//**
+ * @brief Message logging operator
+ *
+ * @param[in] msgFormat Pointer to message format.
+ * @param[in] ... Message to put into log.
+ *
+ * This operator provides message logging capabilities in C style. It expects
+ * a C-style format string followed by an arbitrary number of arguments that
+ * is used to fill the format.
+ *
+ * @todo A static buffer of length 8192 is allocated. Although this is likely
+ * sufficient for any normal application there is nothing that prevents in
+ * principle the overflow of the buffer. A more sophisticated scheme should be
+ * implemented.
+ ******************************************************************************/
+void GLog::operator()(const char *msgFormat, ...)
+{
+    // Allocate message buffer
+    char buffer[8192];
+
+    // Put message into buffer
+    va_list vl;
+    va_start(vl, msgFormat);
+    vsprintf(buffer, msgFormat, vl);
+    va_end(vl);
+
+    // Append message to string buffer
+    m_buffer.append(buffer);
+    m_buffer.append("\n");
+    
+    // Flush buffer
+    flush(true);
+
+    // Return
+    return;
+
+}
+
+
+/***********************************************************************//**
+ * @brief Insert string into output stream
+ *
+ * @param[in] value String to be inserted.
+ ***************************************************************************/
+GLog& GLog::operator<<(const std::string& value)
+{
+    // Add string to buffer
+    m_buffer.append(value);
+
+    // Flush buffer
+    flush();
+
+    // Return logger
+    return *this;
+}
+
+
+/***********************************************************************//**
+ * @brief Insert bool into output stream
+ *
+ * @param[in] value Boolean to be inserted.
+ ***************************************************************************/
+GLog& GLog::operator<<(const bool& value)
+{
+    // Add boolean to buffer
+    if (value)
+        m_buffer.append("true");
+    else
+        m_buffer.append("false");
+
+    // Flush buffer
+    flush();
+
+    // Return logger
+    return *this;
+}
+
+
+/***********************************************************************//**
+ * @brief Insert integer into output stream
+ *
+ * @param[in] value Integer to be inserted.
+ ***************************************************************************/
+GLog& GLog::operator<<(const int& value)
+{
+    // Allocate output stream
+    std::ostringstream oss;
+
+    // Put value into stream
+    oss << value;
+
+    // Append stream to buffer
+    m_buffer.append(oss.str());
+
+    // Flush buffer
+    flush();
+
+    // Return logger
+    return *this;
+}
+
+
+/***********************************************************************//**
+ * @brief Insert double precision value into output stream
+ *
+ * @param[in] value Double precision value to be inserted.
+ ***************************************************************************/
+GLog& GLog::operator<<(const double& value)
+{
+    // Allocate output stream
+    std::ostringstream oss;
+
+    // Put value into stream
+    oss << value;
+
+    // Append stream to buffer
+    m_buffer.append(oss.str());
+
+    // Flush buffer
+    flush();
+
+    // Return logger
+    return *this;
+}
+
+
+/***********************************************************************//**
+ * @brief Insert character value into output stream
+ *
+ * @param[in] value Character value to be inserted.
+ ***************************************************************************/
+GLog& GLog::operator<<(const char& value)
+{
+    // Allocate output stream
+    std::ostringstream oss;
+
+    // Put value into stream
+    oss << value;
+
+    // Append stream to buffer
+    m_buffer.append(oss.str());
+
+    // Flush buffer
+    flush();
+
+    // Return logger
+    return *this;
+}
+
+
+/***********************************************************************//**
+ * @brief Ostream function output operator
+ *
+ * @param[in] fn Ostream function.
+ *
+ * The function output operator is interpreted as a line feed operator.
+ *
+ * @todo This is a quick an dirty implementation of the std::endl operator.
+ * A clean GLog::endl operator should be implemented instead, yet this needs
+ * some deeper understanding of our the 
+ ***************************************************************************/
+GLog& GLog::operator<<(std::ostream& (*fn)(std::ostream&))
+{
+    // Append CR to buffer
+    m_buffer.append("\n");
+
+    // Flush buffer
+    flush(true);
+
+    // Return output stream
+    return *this;
+}
+
+
 /*==========================================================================
  =                                                                         =
  =                             Public methods                              =
  =                                                                         =
  ==========================================================================*/
+
+/***********************************************************************//**
+ * @brief Clear object
+ ***************************************************************************/
+void GLog::clear(void)
+{
+    // Free members
+    free_members();
+
+    // Init members
+    init_members();
+
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Return the number of characters that are actually in the buffer
+ ***************************************************************************/
+int GLog::size(void) const
+{
+    // Return
+    return m_buffer.size();
+}
+
+
+/***********************************************************************//**
+ * @brief Open log file
+ *
+ * @param[in] filename Name of log file.
+ * @param[in] clobber If true overwrite any existing file, otherwise append.
+ *
+ * Opens a file for logging. If a log file was already open it is closed
+ * before opening a new file.
+ ***************************************************************************/
+void GLog::open(const std::string& filename, bool clobber)
+{
+    // Close any existing file
+    close();
+
+    // Open file
+    if (clobber)
+        m_file = fopen(filename.c_str(), "w");
+    else
+        m_file = fopen(filename.c_str(), "a");
+
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Close log file
+ *
+ * Close log file. This method even works if no file has been opened. In any
+ * case flushing of the buffer is enforced.
+ ***************************************************************************/
+void GLog::close(void)
+{
+    // Flush buffer
+    flush(true);
+
+    // Close any open file
+    if (m_file != NULL) {
+        fclose(m_file);
+        m_file = NULL;
+    }
+
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Set data flag that controls date prefixing
+ *
+ * @param[in] flag Enable/disable date flag (true/false). 
+ ***************************************************************************/
+void GLog::date(bool flag)
+{
+    // Set flag
+    m_use_date = flag;
+
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Enables/disables logging into standard output stream
+ *
+ * @param[in] flag Enable/disable logging (true/false).
+ *
+ * Enables or disables logging into the standard output stream.
+ ***************************************************************************/
+void GLog::cout(bool flag)
+{
+    // Set flag
+    m_stdout = flag;
+
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Enables/disables logging into standard error stream
+ *
+ * @param[in] flag Enable/disable logging (true/false).
+ *
+ * Enables or disables logging into the standard error stream.
+ ***************************************************************************/
+void GLog::cerr(bool flag)
+{
+    // Set flag
+    m_stderr = flag;
+
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Set name to put into prefix
+ *
+ * @param[in] name Name to put into prefix. 
+ ***************************************************************************/
+void GLog::name(const std::string& name)
+{
+    // Set name
+    m_name = name;
+
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Set the maximum buffer size
+ *
+ * @param[in] size Maximum buffer size.
+ *
+ * Set the maximum number of characters allowed in the internal buffer before
+ * the buffer is flushed into the specified streams or file.
+ *
+ * @todo No maximum buffer size checking is performed. This could be
+ * implemented but is not really crucial since the physical buffer size can
+ * indeed be larger than this maximum size.
+ ***************************************************************************/
+void GLog::max_size(int size)
+{
+    // Set size
+    m_max_length = size;
+
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Set indentation
+ *
+ * @param[in] indent Indentation in number of characters.
+ *
+ * Set the indentation of the logger. This specifies the number of whitespace
+ * characters that is added between the prefix and the log message of each
+ * line. Indentation allows for easy formatting of text in the log file.
+ ***************************************************************************/
+void GLog::indent(int indent)
+{
+    // Set size
+    m_indent = indent;
+
+    // Return
+    return;
+}
+
 
 /*==========================================================================
  =                                                                         =
@@ -128,6 +510,15 @@ GLog& GLog::operator= (const GLog& log)
 void GLog::init_members(void)
 {
     // Initialise members
+    m_max_length = 8192;
+    m_indent     = 0;
+    m_stdout     = false;
+    m_stderr     = false;
+    m_use_date   = false;
+    m_newline    = true;
+    m_file       = NULL;
+    m_name.clear();
+    m_buffer.clear();
   
     // Return
     return;
@@ -142,6 +533,15 @@ void GLog::init_members(void)
 void GLog::copy_members(const GLog& log)
 {
     // Copy attributes
+    m_max_length = log.m_max_length;
+    m_indent     = log.m_indent;
+    m_stdout     = log.m_stdout;
+    m_stderr     = log.m_stderr;
+    m_use_date   = log.m_use_date;
+    m_newline    = log.m_newline;
+    m_name       = log.m_name;
+    m_buffer     = log.m_buffer;
+    m_file       = log.m_file;
     
     // Return
     return;
@@ -150,11 +550,175 @@ void GLog::copy_members(const GLog& log)
 
 /***********************************************************************//**
  * @brief Delete class members
+ *
+ * Deletes class member after flushing the buffer and closing the log file.
  ***************************************************************************/
 void GLog::free_members(void)
 {
+    // Flush buffer
+    flush(true);
+
+    // Close log file
+    close();
+
     // Return
     return;
+}
+
+
+/***********************************************************************//**
+ * @brief Flush string buffer into log file
+ *
+ * @param[in] force If true, force flushing.
+ *
+ * Flush string buffer if it is full or if flushing is enforced. This method
+ * writes the output string in the relevant streams. It decomposes the
+ * buffer in lines that a separated by a '\n' character and preprends, if
+ * requested, the current date and name. The following streams are currently
+ * implemented (and will be filled in parallel): stdout, stderr, and an ASCII
+ * file.
+ ***************************************************************************/
+void GLog::flush(bool force)
+{
+    // Check if buffer should be flushed
+    bool flush = (force || size() > m_max_length);
+
+    // Flush buffer
+    if (flush) {
+
+        // Flush buffer until it is empty
+        while (m_buffer.size() > 0) {
+
+            // Initialise newline for next line
+            bool newline = m_newline;
+
+            // Find next CR
+            std::string line;
+            size_t pos = m_buffer.find_first_of("\n", 0);
+            if (pos == std::string::npos) {
+                line    = m_buffer;
+                newline = false;
+                m_buffer.clear();
+            }
+            else {
+                line     = m_buffer.substr(0, pos) + "\n";
+                m_buffer = m_buffer.substr(pos+1, m_buffer.size()-pos);
+                newline  = true;
+            }
+
+            // Prepend prefix if we are at the beginning of a new line
+            if (m_newline) {
+                std::string prefix = "";
+                if (m_use_date)
+                    prefix.append(strdate());
+                if (m_name.length() > 0)
+                    prefix.append(" "+m_name);
+                if (prefix.length() > 0)
+                    prefix.append(": ");
+                line = prefix + fill(" ", m_indent) + line;
+            }
+
+            // Put line into requested streams
+            if (m_stdout)
+                std::cout << line;
+            if (m_stderr)
+                std::cerr << line;
+            if (m_file != NULL)
+                fprintf(m_file, line.c_str());
+
+            // Set newline flag for next line
+            m_newline = newline;
+
+        } // endwhile: flush until empty
+
+    } // endif: flush was required
+
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Write string as header into logger
+ *
+ * @param[in] arg Header string.
+ * @param[in] level Header level (0,1,...).
+ *
+ * Writes a string as header into the logger. Various header levels exist:
+ * 0: 80 character wide centred header
+ * 1: Header framed by '=' characters
+ * 2: Header framed by '-' characters
+ ***************************************************************************/
+void GLog::header(const std::string& arg, int level)
+{
+    // Declare frame and text strings
+    std::string frame;
+    std::string text;
+
+    // Create level dependent header strings
+    switch (level) {
+    case 0:
+        break;
+    case 1:
+    case 2:
+        text  = "| " + arg + " |";
+        frame = (level == 1) ? "+" +  fill("=", text.length()-2) + "+"
+                             : "+" +  fill("-", text.length()-2) + "+";
+        break;
+    default:
+        break;
+    }
+
+    // Write header into logger
+    if (text.length() > 0) {
+        m_buffer.append(frame);
+        m_buffer.append("\n");
+        m_buffer.append(text);
+        m_buffer.append("\n");
+        m_buffer.append(frame);
+        m_buffer.append("\n");
+        flush(true);
+    }
+
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Return current date
+ *
+ * Returns the current date as string in the format yyyy-mm-ddThh:mm:ss.
+ ***************************************************************************/
+std::string GLog::strdate(void)
+{
+    // Allocate variables
+    struct tm timeStruct;
+    time_t    now;
+    char      buffer[100];
+
+    // Get time
+    now = time(NULL);
+    #ifdef HAVE_GMTIME_R   
+    gmtime_r(&now, &timeStruct);
+    #else
+    memcpy(&timeStruct, gmtime(&now), sizeof(struct tm));
+    #endif
+
+    // Write message type, time and task name to buffer
+    sprintf(buffer, "%04d-%02d-%02dT%02d:%02d:%02d",
+                    timeStruct.tm_year + 1900,
+                    timeStruct.tm_mon + 1,
+                    timeStruct.tm_mday,
+                    timeStruct.tm_hour,
+                    timeStruct.tm_min,
+                    timeStruct.tm_sec);
+
+    // Build string from buffer
+    std::string date = buffer;
+
+    // Return date
+    return date;
 }
 
 
@@ -163,20 +727,3 @@ void GLog::free_members(void)
  =                                 Friends                                 =
  =                                                                         =
  ==========================================================================*/
-
-/***********************************************************************//**
- * @brief Put object in output stream
- *
- * @param[in] os Output stream into which the model will be dumped
- * @param[in] log Object to be dumped
- ***************************************************************************/
-std::ostream& operator<< (std::ostream& os, const GLog& log)
-{
-    // Put object in stream
-    os << "=== GLog ===" << std::endl;
-
-    // Return output stream
-    return os;
-}
-
-
