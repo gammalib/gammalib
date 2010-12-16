32504de89707e1eeaa8efa394558b614c97efb17
knodlseder <>
2010-12-16 23:58:47 +0000

    implement PSF v1

--- inst/lat/include/GLATPsf.hpp
+++ inst/lat/include/GLATPsf.hpp
@@ -23,6 +23,7 @@
 #include <string>
 #include <iostream>
 #include "GLog.hpp"
+#include "GLATInstDir.hpp"
 #include "GLATPointing.hpp"
 #include "GLATResponseTable.hpp"
 #include "GFits.hpp"
@@ -30,6 +31,7 @@
 #include "GSkyDir.hpp"
 #include "GEnergy.hpp"
 #include "GTime.hpp"
+#include "GIntegrand.hpp"
 
 
 /***********************************************************************//**
@@ -52,9 +54,11 @@ public:
 
     // Operators
     GLATPsf& operator= (const GLATPsf& psf);
-    //double    operator() (const double& logE, const double& ctheta);
-    //double    operator() (const GSkyDir& srcDir, const GEnergy& srcEng,
-    //                      const GTime& srcTime, const GLATPointing& pnt);
+    double   operator() (const double& offset, const double& logE,
+                         const double& ctheta);
+    double   operator() (const GLATInstDir& obsDir, const GSkyDir&  srcDir,
+                         const GEnergy& srcEng, const GTime& srcTime,
+                         const GLATPointing& pnt);
 
     // Methods
     void         clear(void);
@@ -66,30 +70,65 @@ public:
     int          size(void) const { return nenergies()*ncostheta(); }
     int          nenergies(void) const { return m_rpsf_bins.nenergies(); }
     int          ncostheta(void) const { return m_rpsf_bins.ncostheta(); }
-    //double       costhetamin(void) const { return m_min_ctheta; }
-    //void         costhetamin(const double& ctheta);
-    //void         ltcube_energy(const GEnergy& energy);
-    //double       ltcube_ctheta(const double& costheta);
-    //double       ltcube_ctheta_phi(const double& costheta, const double& phi);
+    double       costhetamin(void) const { return m_min_ctheta; }
+    void         costhetamin(const double& ctheta);
     bool         hasphi(void) const { return false; }
+    bool         isfront(void) const { return m_front; }
+    bool         isback(void) const { return !m_front; }
+    int          version(void) const { return m_version; }
     std::string  print(void) const;
 
 private:
     // Methods
-    void init_members(void);
-    void copy_members(const GLATPsf& psf);
-    void free_members(void);
-    void read_psf(const GFitsTable* hdu);
-    void write_psf(GFits& file) const;
+    void   init_members(void);
+    void   copy_members(const GLATPsf& psf);
+    void   free_members(void);
+    void   read_scale(const GFitsTable* hdu);
+    void   write_scale(GFits& file) const;
+    double scale_factor(const double& energy) const;
+
+    // PSF version 1 methods and classes
+    void          read_psf_v1(const GFitsTable* hdu);
+    void          write_psf_v1(GFits& file) const;
+    double        psf_v1(const double& offset, const double& logE,
+                         const double& ctheta);
+    static double base_fct_v1(const double& u, const double& gamma);
+    static double base_int_v1(const double& u, const double& gamma);
+    class base_integrand_v1 : public GIntegrand {
+    public:
+        base_integrand_v1(double ncore, double ntail, double sigma,
+                          double gcore, double gtail) :
+                          m_ncore(ncore), m_ntail(ntail), m_sigma(sigma),
+                          m_gcore(gcore), m_gtail(gtail) { }
+        double eval(double x) {
+            double r = x / m_sigma;
+            double u = 0.5 * r * r;
+            double f = m_ncore * base_fct_v1(u, m_gcore) +
+                       m_ntail * base_fct_v1(u, m_gtail);
+            return (f*sin(x));
+        }
+    private:
+        double m_ncore;
+        double m_ntail;
+        double m_sigma;
+        double m_gcore;
+        double m_gtail;
+    };
     
     // Protected members
+    int                 m_version;      //!< PSF version (starting from 1)
+    bool                m_front;        //!< PSF is for front section?
     GLATResponseTable   m_rpsf_bins;    //!< PSF energy and cos theta binning
+    double              m_scale_par1;   //!< PSF scaling parameter 1
+    double              m_scale_par2;   //!< PSF scaling parameter 2
+    double              m_scale_index;  //!< PSF scaling index
+    double              m_min_ctheta;   //!< Minimum valid cos(theta)
+
+    // PSF version 1 protected members
     std::vector<double> m_ncore;        //!< PSF ncore parameter
     std::vector<double> m_sigma;        //!< PSF sigma parameter
     std::vector<double> m_gcore;        //!< PSF gcore parameter
     std::vector<double> m_gtail;        //!< PSF gtail parameter
-    std::vector<double> m_scale;        //!< PSF scaling parameters
-    double              m_ltcube_logE;  //!< log10 energy for ltcube methods
 };
 
 #endif /* GLATPSF_HPP */
--- inst/lat/src/GLATPsf.cpp
+++ inst/lat/src/GLATPsf.cpp
@@ -27,30 +27,23 @@
 #include "GTools.hpp"
 #include "GFitsBinTable.hpp"
 #include "GFitsTableFloatCol.hpp"
+#include "GIntegral.hpp"
 
 /* __ Method name definitions ____________________________________________ */
+#define G_OP_PSF1           "GLATPsf::operator() (double&, double&, double&)"
 #define G_READ                             "GLATPsf::read(const GFits* file)"
 #define G_READ_PSF                           "GLATPsf::read_psf(GFitsTable*)"
+#define G_READ_SCALE                       "GLATPsf::read_scale(GFitsTable*)"
 
 /* __ Macros _____________________________________________________________ */
 
 /* __ Coding definitions _________________________________________________ */
 
 /* __ Debug definitions __________________________________________________ */
+#define G_CHECK_PSF_NORM 0                       //!< Check PSF normalization
 
 /* __ Constants __________________________________________________________ */
-/*
-const double scale_factor_log = 39.810717;
-const double scale_base_log   = 0.15848932;
-const double scale_front_c0   = 3.77e-4;
-const double scale_front_c1   = 5.8e-2;
-const double scale_back_c0    = 1.3e-3;
-const double scale_back_c1    = 9.6e-2;
-const int    angle_num        = 5000;    // Number of angles for PSF
-const double angle_min        = 0.0001;  // Minimum angular separation (rad)
-const double angle_bin        = 0.01;    // Angular separation binning (rad)
-const double max_sep          = pihalf;  // Maximum angular separation (rad)
-*/
+
 
 /*==========================================================================
  =                                                                         =
@@ -154,6 +147,66 @@ GLATPsf& GLATPsf::operator= (const GLATPsf& psf)
 }
 
 
+/***********************************************************************//**
+ * @brief Return point spread function value
+ *
+ * @param[in] offset Offset angle (deg).
+ * @param[in] logE Log10 of the true photon energy (MeV).
+ * @param[in] ctheta Cosine of zenith angle.
+ *
+ * @exception GLATException::invalid_response
+ *            Invalid response function version.
+ *
+ * This operator returns the version dependent value of the point spread
+ * function.
+ *
+ * @todo Implement also PSF versions 2 and 3
+ ***************************************************************************/
+double GLATPsf::operator() (const double& offset, const double& logE,
+                            const double& ctheta)
+{
+    // Initialise response
+    double psf = 0.0;
+
+    // Get point spread function value
+    switch (m_version) {
+    case 1:
+        psf = psf_v1(offset, logE, ctheta);
+        break;
+    default:
+        throw GLATException::invalid_response(G_OP_PSF1, 
+              "Unsupported response function version "+str(m_version)+".");
+        break;
+    }
+
+    // Return point spread function
+    return psf;
+}
+
+
+/***********************************************************************//**
+ * @brief Return point spread function
+ *
+ * @param[in] obsDir Observed photon direction.
+ * @param[in] srcDir True photon direction.
+ * @param[in] srcEng True energy of photon.
+ * @param[in] srcTime True photon arrival time.
+ * @param[in] pnt LAT pointing.
+ *
+ * @todo Not yet implemented.
+ ***************************************************************************/
+double GLATPsf::operator() (const GLATInstDir& obsDir, const GSkyDir& srcDir,
+                            const GEnergy& srcEng, const GTime& srcTime,
+                            const GLATPointing& pnt)
+{
+    // Initialise response
+    double psf = 0.0;
+
+    // Return point spread function
+    return psf;
+}
+
+
 /*==========================================================================
  =                                                                         =
  =                             Public methods                              =
@@ -209,7 +262,7 @@ void GLATPsf::load(const std::string filename)
  * @brief Save point spread function into FITS file
  *
  * @param[in] filename FITS file.
- * @param[in] clobber Overwrite existing file?.
+ * @param[in] clobber Overwrite existing file?
  ***************************************************************************/
 void GLATPsf::save(const std::string filename, bool clobber)
 {
@@ -233,25 +286,60 @@ void GLATPsf::save(const std::string filename, bool clobber)
  * @param[in] fits FITS file.
  *
  * @exception GException::fits_hdu_not_found
- *            Effective area HDU not found in FITS file
+ *            Response tables not found in FITS file
+ * @exception GException::invalid_response
+ *            Invalid response type or unsupported response version found.
  *
- * @todo Implement reading of scaling parameters
+ * Reads the version dependent point spread function from the FITS file.
+ *
+ * @todo Implement also PSF versions 2 and 3
  ***************************************************************************/
 void GLATPsf::read(const GFits& fits)
 {
     // Clear instance
     clear();
 
-    // Get pointer to effective area HDU
+    // Get pointer to PSF parameters table
     GFitsTable* hdu_rpsf  = fits.table("RPSF");
-    GFitsTable* hdu_scale = fits.table("PSF_SCALING_PARAMS");
     if (hdu_rpsf == NULL)
         throw GException::fits_hdu_not_found(G_READ, "RPSF");
+
+    // Get pointer to PSF scaling parameters table
+    GFitsTable* hdu_scale = fits.table("PSF_SCALING_PARAMS");
     if (hdu_scale == NULL)
         throw GException::fits_hdu_not_found(G_READ, "PSF_SCALING_PARAMS");
 
+    // Determine PSF version
+    try {
+        m_version = hdu_rpsf->integer("PSFVER");
+    }
+    catch (GException::fits_key_not_found) {
+        m_version = 1;
+    }
+
+    // Determine PSF type
+    std::string detnam = strip_whitespace(toupper(hdu_rpsf->string("DETNAM")));
+    if (detnam == "FRONT")
+        m_front = true;
+    else if (detnam == "BACK")
+        m_front = false;
+    else
+        throw GLATException::invalid_response(G_READ, 
+              "Unknown response type "+detnam+".");
+
     // Read point spread function
-    read_psf(hdu_rpsf);
+    switch (m_version) {
+    case 1:
+        read_psf_v1(hdu_rpsf);
+        break;
+    default:
+        throw GLATException::invalid_response(G_READ, 
+              "Unsupported response function version "+str(m_version)+".");
+        break;
+    }
+
+    // Read scaling parameters
+    read_scale(hdu_scale);
 
     // Return
     return;
@@ -262,11 +350,39 @@ void GLATPsf::read(const GFits& fits)
  * @brief Write point spread function into FITS file
  *
  * @param[in] fits FITS file.
+ *
+ * @todo Implement also PSF versions 2 and 3
  ***************************************************************************/
 void GLATPsf::write(GFits& fits) const
 {
-    // Write energy dispersion
-    write_psf(fits);
+    // Write point spread function
+    switch (m_version) {
+    case 1:
+        write_psf_v1(fits);
+        break;
+    default:
+        throw GLATException::invalid_response(G_READ, 
+              "Unsupported response function version "+str(m_version)+".");
+        break;
+    }
+
+    // Write scaling parameters
+    write_scale(fits);
+
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Set minimum cos(theta) angle for point spread function access
+ *
+ * @param[in] ctheta Cosine of maximum zenith angle.
+ ***************************************************************************/
+void GLATPsf::costhetamin(const double& ctheta)
+{
+    // Set minimum cos(theta) value
+    m_min_ctheta = ctheta;
 
     // Return
     return;
@@ -283,8 +399,16 @@ std::string GLATPsf::print(void) const
 
     // Append header
     result.append("=== GLATPsf ===");
-    result.append("\n"+parformat("Number of energy bins")+str(nenergies()));
-    result.append("\n"+parformat("Number of cos theta bins")+str(ncostheta()));
+    result.append("\n"+parformat("Version")+str(version()));
+    result.append("\n"+parformat("Detector section"));
+    if (isfront())
+        result.append("front");
+    else
+        result.append("back");
+    result.append("\n"+parformat("Energy scaling"));
+    result.append("sqrt(");
+    result.append("("+str(m_scale_par1)+"*(E/100)^"+str(m_scale_index)+")^2");
+    result.append(" + ("+str(m_scale_par2)+")^2)");
 
     // Return result
     return result;
@@ -303,13 +427,19 @@ std::string GLATPsf::print(void) const
 void GLATPsf::init_members(void)
 {
     // Initialise members
+    m_version = 1;
+    m_front   = true;
     m_rpsf_bins.clear();
+    m_scale_par1  = 0.0;
+    m_scale_par2  = 0.0;
+    m_scale_index = 0.0;
+    m_min_ctheta  = 0.0;
+
+    // Initialise PSF version 1 members
     m_ncore.clear();
     m_sigma.clear();
     m_gcore.clear();
     m_gtail.clear();
-    m_scale.clear();
-    m_ltcube_logE = 0.0;
 
     // Return
     return;
@@ -324,13 +454,19 @@ void GLATPsf::init_members(void)
 void GLATPsf::copy_members(const GLATPsf& psf)
 {
     // Copy members
+    m_version     = psf.m_version;
+    m_front       = psf.m_front;
     m_rpsf_bins   = psf.m_rpsf_bins;
+    m_scale_par1  = psf.m_scale_par1;
+    m_scale_par2  = psf.m_scale_par2;
+    m_scale_index = psf.m_scale_index;
+    m_min_ctheta  = psf.m_min_ctheta;
+
+    // Copy PSF version 1 members
     m_ncore       = psf.m_ncore;
     m_sigma       = psf.m_sigma;
     m_gcore       = psf.m_gcore;
     m_gtail       = psf.m_gtail;
-    m_scale       = psf.m_scale;
-    m_ltcube_logE = psf.m_ltcube_logE;
 
     // Return
     return;
@@ -348,16 +484,111 @@ void GLATPsf::free_members(void)
 
 
 /***********************************************************************//**
- * @brief Read point spread function from FITS table
+ * @brief Read PSF scale factors from FITS table
  *
  * @param[in] hdu FITS table pointer.
  *
  * @exception GException::fits_column_not_found
- *            Effective area column not found
+ *            Table column not found
+ ***************************************************************************/
+void GLATPsf::read_scale(const GFitsTable* hdu)
+{
+    // Get pointer to column
+    GFitsTableCol* scale = ((GFitsTable*)hdu)->column("PSFSCALE");
+    if (scale == NULL)
+        throw GException::fits_column_not_found(G_READ_SCALE, "PSFSCALE");
+
+    // Get scaling factors
+    if (isfront()) {
+        m_scale_par1 = scale->real(0,0);
+        m_scale_par2 = scale->real(0,1);
+    }
+    else {
+        m_scale_par1 = scale->real(0,2);
+        m_scale_par2 = scale->real(0,3);
+    }
+    m_scale_index = scale->real(0,4);
+
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Write PSF scale factors
+ *
+ * @param[in] file FITS file.
+ ***************************************************************************/
+void GLATPsf::write_scale(GFits& file) const
+{
+    // Create new binary table
+    GFitsBinTable* hdu_scale = new GFitsBinTable;
+
+    // Set table attributes
+    hdu_scale->extname("PSF_SCALING_PARAMS");
+
+    // Allocate floating point vector column
+    GFitsTableFloatCol col_scale = GFitsTableFloatCol("PSFSCALE",  1, 5);
+
+    // Fill columns
+    if (isfront()) {
+        col_scale(0,0) = m_scale_par1;
+        col_scale(0,1) = m_scale_par2;
+        col_scale(0,2) = 0.0;
+        col_scale(0,3) = 0.0;
+    }
+    else {
+        col_scale(0,0) = 0.0;
+        col_scale(0,1) = 0.0;
+        col_scale(0,2) = m_scale_par1;
+        col_scale(0,3) = m_scale_par2;
+    }
+    col_scale(0,4) = m_scale_index;
+
+    // Append column to table
+    hdu_scale->append_column(col_scale);
+        
+    // Append HDU to FITS file
+    file.append(hdu_scale);
+
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Return scale factor for energy (in MeV)
+ *
+ * @param[in] energy Photon energy (in MeV).
+ ***************************************************************************/
+double GLATPsf::scale_factor(const double& energy) const
+{
+    // Compute scale factor
+    double f1       = m_scale_par1 * pow(0.01*energy, m_scale_index);
+    double scale    = sqrt(f1*f1 + m_scale_par2*m_scale_par2);
+
+    // Return scale factor
+    return scale;
+}
+
+
+/*==========================================================================
+ =                                                                         =
+ =                             PSF Version 1                               =
+ =                                                                         =
+ ==========================================================================*/
+
+/***********************************************************************//**
+ * @brief Read point spread function from FITS table (version 1)
+ *
+ * @param[in] hdu FITS table pointer.
+ *
+ * @exception GException::fits_column_not_found
+ *            Table column not found
  * @exception GLATException::inconsistent_response
  *            Inconsistent response table encountered
  ***************************************************************************/
-void GLATPsf::read_psf(const GFitsTable* hdu)
+void GLATPsf::read_psf_v1(const GFitsTable* hdu)
 {
     // Clear arrays
     m_ncore.clear();
@@ -367,6 +598,9 @@ void GLATPsf::read_psf(const GFitsTable* hdu)
 
     // Get energy and cos theta binning
     m_rpsf_bins.read(hdu);
+
+    // Set minimum cos(theta)
+    m_min_ctheta = m_rpsf_bins.costheta_lo(0);
 
     // Continue only if there are bins
     int size = m_rpsf_bins.size();
@@ -422,13 +656,13 @@ void GLATPsf::read_psf(const GFitsTable* hdu)
 
 
 /***********************************************************************//**
- * @brief Write point spread function into FITS file
+ * @brief Write point spread function into FITS file (version 1)
  *
  * @param[in] file FITS file.
  *
  * This method does not write anything if the instance is empty.
  ***************************************************************************/
-void GLATPsf::write_psf(GFits& file) const
+void GLATPsf::write_psf_v1(GFits& file) const
 {
     // Continue only if there are bins
     int size = m_rpsf_bins.size();
@@ -470,6 +704,127 @@ void GLATPsf::write_psf(GFits& file) const
 
     // Return
     return;
+}
+
+
+/***********************************************************************//**
+ * @brief Return point spread function value (version 1)
+ *
+ * @param[in] offset Offset angle (deg).
+ * @param[in] logE Log10 of the true photon energy (MeV).
+ * @param[in] ctheta Cosine of zenith angle.
+ ***************************************************************************/
+double GLATPsf::psf_v1(const double& offset, const double& logE,
+                       const double& ctheta)
+{
+    // Set constants
+    const double ub = 10.0;
+
+    // Initialise response
+    double psf = 0.0;
+
+    // Compute point spread function
+    if (ctheta >= m_min_ctheta) {
+
+        // Get response parameters
+        double ncore = m_rpsf_bins.interpolate(logE, ctheta, m_ncore);
+        double sigma = m_rpsf_bins.interpolate(logE, ctheta, m_sigma);
+        double gcore = m_rpsf_bins.interpolate(logE, ctheta, m_gcore);
+        double gtail = m_rpsf_bins.interpolate(logE, ctheta, m_gtail);
+
+        // Compute energy in MeV
+        double energy = pow(10.0, logE);
+
+        // Rescale the sigma value after interpolation
+        sigma *= scale_factor(energy);
+
+        // Compute base function argument
+        double r = deg2rad * offset / sigma;
+        double u = 0.5 * r * r;
+
+        // Compute normalization of tail
+        double ntail = ncore * (base_fct_v1(ub, gcore) / base_fct_v1(ub, gtail));
+
+        // Ensure that PSF integrates to unity. For small energies perform
+        // a numerical integration over the solid angle, while for larger
+        // energies use a small angle approximation.
+        if (energy < 120.0) {
+            GLATPsf::base_integrand_v1 integrand(ncore, ntail, sigma, gcore, gtail);
+            GIntegral integral(&integrand);
+            ncore /= integral.romb(0.0, pihalf) * twopi;
+        }
+        else {
+            double rmax = pihalf / sigma;
+            double umax = 0.5 * rmax * rmax;
+            double norm = ncore*base_int_v1(umax, gcore) + ntail*base_int_v1(umax, gtail);
+            ncore /= norm * twopi * sigma * sigma;
+        }
+
+        // Re-compute normalization of tail
+        ntail = ncore * (base_fct_v1(ub, gcore) / base_fct_v1(ub, gtail));
+
+        // Compute PSF value
+        psf = ncore * base_fct_v1(u, gcore) + ntail * base_fct_v1(u, gtail);
+
+        // Compile option: check PSF normalization
+        #if G_CHECK_PSF_NORM
+        GLATPsf::base_integrand_v1 integrand(ncore, ntail, sigma, gcore, gtail);
+        GIntegral integral(&integrand);
+        double sum = integral.romb(0.0, pihalf) * twopi;
+        std::cout << "Energy=" << energy;
+        std::cout << " Offset=" << offset;
+        std::cout << " cos(theta)=" << ctheta;
+        std::cout << " error=" << sum-1.0 << std::endl;
+        #endif
+    }
+
+    // Return point spread function
+    return psf;
+}
+
+
+/***********************************************************************//**
+ * @brief Return point spread base function value (version 1)
+ *
+ * @param[in] u Function argument.
+ * @param[in] gamma Index.
+ *
+ * The version 1 PSF base function is given by
+ * \f[\left(1 - \frac{1}{\Gamma} \right)
+ *    \left(1 + \frac{u}{\Gamma} \right)^{-\Gamma}\f]
+ ***************************************************************************/
+double GLATPsf::base_fct_v1(const double& u, const double& gamma)
+{
+    // Get base function value. The special case of gamma==1 is a ugly
+    // kluge because of sloppy programming in handoff response when
+    // setting boundaries of fit parameters for the PSF.
+    double base = (gamma == 1)
+                  ? (1.0 - 1.0/1.001) * pow(1.0 + u/1.001, -1.001)
+                  : (1.0 - 1.0/gamma) * pow(1.0 + u/gamma, -gamma);
+
+    // Return base function
+    return base;
+}
+
+
+/***********************************************************************//**
+ * @brief Return point spread base function integral (version 1)
+ *
+ * @param[in] u Function argument.
+ * @param[in] gamma Index.
+ *
+ * The version 1 PSF base function integral is approximated by
+ * \f[1 - \left(1 + \frac{u}{\Gamma} \right)^{1-\Gamma}\f]
+ * which is valid for small angles \f$u\f$. For larger angles a numerical
+ * integration of the base function has to be performed.
+ ***************************************************************************/
+double GLATPsf::base_int_v1(const double& u, const double& gamma)
+{
+    // Compute integral of base function
+    double integral = 1.0 - pow(1.0 + u/gamma, 1.0 - gamma);
+
+    // Return integral
+    return integral;
 }
 
 
