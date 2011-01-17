9985607ea0b0ca80fa23f83cbeada57f5522b8ed
knodlseder <>
2011-01-17 22:16:09 +0000

    initial version

---
+++ include/GPhoton.hpp
@@ -0,0 +1,98 @@
+/***************************************************************************
+ *                        GPhoton.hpp - Photon class                       *
+ * ----------------------------------------------------------------------- *
+ *  copyright (C) 2011 by Jurgen Knodlseder                                *
+ * ----------------------------------------------------------------------- *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+/**
+ * @file GPhoton.hpp
+ * @brief Photon class definition.
+ * @author J. Knodlseder
+ */
+
+#ifndef GPHOTON_HPP
+#define GPHOTON_HPP
+
+/* __ Includes ___________________________________________________________ */
+#include <string>
+#include <iostream>
+#include "GLog.hpp"
+#include "GSkyDir.hpp"
+#include "GEnergy.hpp"
+#include "GTime.hpp"
+
+
+/***********************************************************************//**
+ * @class GPhoton
+ *
+ * @brief Class that handles photons.
+ *
+ * The GPhoton class stores the physical attributes of a photon such as the
+ * photon arrival direction, its energy and its arrival time. This class is
+ * mainly used for Monte Carlo simulations.
+ ***************************************************************************/
+class GPhoton {
+
+    // I/O friends
+    friend std::ostream& operator<< (std::ostream& os, const GPhoton& ph);
+    friend GLog&         operator<< (GLog& log, const GPhoton& ph);
+
+    // Operator friends
+    friend bool operator== (const GPhoton &a, const GPhoton &b);
+    friend bool operator!= (const GPhoton &a, const GPhoton &b);
+
+public:
+    // Constructors and destructors
+    GPhoton(void);
+    GPhoton(const GPhoton& ph);
+    virtual ~GPhoton(void);
+ 
+    // Operators
+    GPhoton& operator= (const GPhoton& ph);
+
+    // Methods
+    void        clear(void);
+    GSkyDir&    dir(void) { return m_dir; }
+    GEnergy&    energy(void) { return m_energy; }
+    GTime&      time(void) { return m_time; }
+    void        dir(const GSkyDir& dir) { m_dir=dir; }
+    void        energy(const GEnergy& energy) { m_energy=energy; }
+    void        time(const GTime& time) { m_time=time; }
+    std::string print(void) const;
+  
+protected:
+    // Protected methods
+    void init_members(void);
+    void copy_members(const GPhoton& ph);
+    void free_members(void);
+
+    // Protected data members
+    GSkyDir m_dir;           //!< Photon arrival direction
+    GEnergy m_energy;        //!< Photon energy
+    GTime   m_time;          //!< Photon arrival time
+};
+
+
+/***************************************************************************
+ *                               Inline friends                            *
+ ***************************************************************************/
+inline
+bool operator== (const GPhoton &a, const GPhoton &b)
+{
+    return (a.m_energy == b.m_energy && a.m_time == b.m_time &&
+            a.m_dir.dist(b.m_dir) == 0.0);
+}
+inline
+bool operator!= (const GPhoton &a, const GPhoton &b)
+{
+    return (a.m_energy != b.m_energy ||  a.m_time != b.m_time ||
+            a.m_dir.dist(b.m_dir) > 0.0);
+}
+
+#endif /* GPHOTON_HPP */
---
+++ src/obs/GPhoton.cpp
@@ -0,0 +1,252 @@
+/***************************************************************************
+ *                        GPhoton.hpp - Photon class                       *
+ * ----------------------------------------------------------------------- *
+ *  copyright (C) 2011 by Jurgen Knodlseder                                *
+ * ----------------------------------------------------------------------- *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+/**
+ * @file GPhoton.hpp
+ * @brief Photon class implementation.
+ * @author J. Knodlseder
+ */
+
+/* __ Includes ___________________________________________________________ */
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+//#include <cfloat>
+//#include <cmath>
+#include "GPhoton.hpp"
+#include "GTools.hpp"
+
+/* __ Constants __________________________________________________________ */
+
+/* __ Method name definitions ____________________________________________ */
+
+/* __ Macros _____________________________________________________________ */
+
+/* __ Coding definitions _________________________________________________ */
+
+/* __ Debug definitions __________________________________________________ */
+
+
+/*==========================================================================
+ =                                                                         =
+ =                         Constructors/destructors                        =
+ =                                                                         =
+ ==========================================================================*/
+
+/***********************************************************************//**
+ * @brief Void constructor
+ ***************************************************************************/
+GPhoton::GPhoton(void)
+{
+    // Initialise private members
+    init_members();
+  
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Copy constructor
+ *
+ * @param[in] ph Photon.
+ ***************************************************************************/
+GPhoton::GPhoton(const GPhoton& ph)
+{ 
+    // Initialise private members
+    init_members();
+
+    // Copy members
+    copy_members(ph);
+
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Destructor
+ ***************************************************************************/
+GPhoton::~GPhoton(void)
+{
+    // Free members
+    free_members();
+
+    // Return
+    return;
+}
+
+
+/*==========================================================================
+ =                                                                         =
+ =                               Operators                                 =
+ =                                                                         =
+ ==========================================================================*/
+
+/***********************************************************************//**
+ * @brief Assignment operator
+ *
+ * @param[in] ph Photon.
+ ***************************************************************************/
+GPhoton& GPhoton::operator= (const GPhoton& ph)
+{ 
+    // Execute only if object is not identical
+    if (this != &ph) {
+
+        // Free members
+        free_members();
+
+        // Initialise private members for clean destruction
+        init_members();
+
+        // Copy members
+        copy_members(ph);
+
+    } // endif: object was not identical
+  
+    // Return
+    return *this;
+}
+
+
+/*==========================================================================
+ =                                                                         =
+ =                             Public methods                              =
+ =                                                                         =
+ ==========================================================================*/
+
+/***********************************************************************//**
+ * @brief Clear instance
+ ***************************************************************************/
+void GPhoton::clear(void)
+{
+    // Free members
+    free_members();
+
+    // Initialise private members
+    init_members();
+    
+    // Return
+    return; 
+}
+
+
+/***********************************************************************//**
+ * @brief Print photon
+ *
+ * @todo Implement print() methods for GTime and GSkyDir
+ ***************************************************************************/
+std::string GPhoton::print(void) const
+{
+    // Initialise result string
+    std::string result;
+
+    // Append header
+    result.append("=== GPhoton ===");
+
+    // Append arrival direction
+    result.append("\n"+parformat("Arrival direction"));
+    result.append("RA="+str(m_dir.ra_deg())+", DEC="+str(m_dir.dec_deg()));
+
+    // Append energy and time
+    result.append("\n"+parformat("Energy")+m_energy.print());
+    result.append("\n"+parformat("Arrival time (MJD)")+str(m_time.mjd()));
+
+    // Return
+    return result;
+}
+
+
+/*==========================================================================
+ =                                                                         =
+ =                             Private methods                             =
+ =                                                                         =
+ ==========================================================================*/
+
+/***********************************************************************//**
+ * @brief Initialise class members
+ ***************************************************************************/
+void GPhoton::init_members(void)
+{
+    // Initialise members
+    m_dir.clear();
+    m_energy.clear();
+    m_time.clear();
+  
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Copy class members
+ *
+ * @param[in] ph Photon.
+ ***************************************************************************/
+void GPhoton::copy_members(const GPhoton& ph)
+{
+    // Copy time
+    m_dir    = ph.m_dir;
+    m_energy = ph.m_energy;
+    m_time   = ph.m_time;
+    
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Delete class members
+ ***************************************************************************/
+void GPhoton::free_members(void)
+{
+    // Return
+    return;
+}
+
+
+/*==========================================================================
+ =                                                                         =
+ =                                 Friends                                 =
+ =                                                                         =
+ ==========================================================================*/
+
+/***********************************************************************//**
+ * @brief Output operator
+ *
+ * @param[in] os Output stream.
+ * @param[in] ph Photon.
+ ***************************************************************************/
+std::ostream& operator<< (std::ostream& os, const GPhoton& ph)
+{
+     // Write photon in output stream
+    os << ph.print();
+
+    // Return output stream
+    return os;
+}
+
+
+/***********************************************************************//**
+ * @brief Log operator
+ *
+ * @param[in] log Logger.
+ * @param[in] ph Photon.
+ ***************************************************************************/
+GLog& operator<< (GLog& log, const GPhoton& ph)
+{
+    // Write photon into logger
+    log << ph.print();
+
+    // Return logger
+    return log;
+}
