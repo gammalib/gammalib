c17a9f17a22d7041cc097fe9560925a4cb711bd5
knodlseder <>
2011-01-19 17:59:35 +0000

    add plotting function that is only called if matplotlib exists

--- inst/cta/test/test_sim.py
+++ inst/cta/test/test_sim.py
@@ -2,30 +2,31 @@
 
 from gammalib import *
 from math import *
-import os
-import numpy as np
-import matplotlib.pyplot as plt
+#import os
+#import numpy as np
 
 
 # ======================== #
 # Simulate CTA observation #
 # ======================== #
-def simulate(xmlname):
+def simulate(xmlname, e_min, e_max, area, duration):
 	"""
 	Simulate CTA observation.
 	"""
-    # Define MC parameters
-	area   = 1.0
-	dir    = GSkyDir()
+	# Allocate MC parameters
+	dir  = GSkyDir()
+	emin = GEnergy()
+	emax = GEnergy()
+	tmin = GTime()
+	tmax = GTime()
+	
+	# Define MC parameters
+	dir.radec_deg(117.0, -33.0)
 	radius = 10.0
-	emin   = GEnergy()
-	emax   = GEnergy()
-	tmin   = GTime()
-	tmax   = GTime()
-	emin.TeV(0.1)
-	emax.TeV(100.0)
+	emin.TeV(e_min)
+	emax.TeV(e_max)
 	tmin.met(0.0)
-	tmax.met(1000.0)
+	tmax.met(duration)
 	
 	# Allocate random number generator
 	ran = GRan()
@@ -38,12 +39,85 @@ def simulate(xmlname):
 	photons = models.mc(area, dir, radius, emin, emax, tmin, tmax, ran)
 	
 	# Print photons
-	print str(len(photons))+" photon simulated."
 	#for photon in photons:
 	#	print photon
+	print str(len(photons))+" photons simulated."
+
+	# Return photons
+	return photons
+
+
+# ============ #
+# Show photons #
+# ============ #
+def show_photons(photons, xmlname, e_min, e_max, area, duration, ebins=50):
+	"""
+	Show photons using matplotlib (if available).
+	"""
+	# Only proceed if matplotlib is available
+	try:
+		# Import matplotlib
+		import matplotlib.pyplot as plt
+
+		# Create figure
+		plt.figure(1)
+		plt.title("MC simulated photon spectrum ("+str(e_min)+'-'+str(e_max)+" TeV)")
+		
+		# Setup energy range covered by data
+		emin = GEnergy()
+		emax = GEnergy()
+		emin.TeV(e_min)
+		emax.TeV(e_max)
+		ebds = GEbounds()
+		ebds.setlog(emin, emax, ebins)
+		
+		# Create energy axis
+		energy = []
+		for i in range(ebds.size()):
+			energy.append(ebds.elogmean(i).TeV())
+
+		# Fill histogram
+		counts = [0.0 for i in range(ebds.size())]
+		for photon in photons:
+			index         = ebds.index(photon.energy())
+			counts[index] = counts[index] + 1.0
+
+		# Create error bars
+		error = [sqrt(c) for c in counts]
+
+		# Get model values
+		models = GModels(xmlname)
+		model = []
+		d = GSkyDir()
+		d.radec_deg(117.0, -33.0)
+		t = GTime()
+		for i in range(ebds.size()):
+			eval   = ebds.elogmean(i)
+			ewidth = ebds.emax(i) - ebds.emin(i)
+			f      = models.value(d, eval, t) * area * duration * ewidth.MeV()
+			model.append(f)
+
+		# Plot data
+		plt.loglog(energy, counts, 'ro')
+		plt.errorbar(energy, counts, error, fmt=None, ecolor='r')
+
+		# Plot model
+		plt.plot(energy, model, 'b-')
+
+		# Set axes
+		plt.xlabel("Energy (TeV)")
+		plt.ylabel("Number of incident photons")
+
+		# Allocate histogram
+		# Show plot
+		plt.show()
+
+	except:
+		print "Matplotlib is not (correctly) installed on your system. No data are shown."
 
 	# Return
 	return
+
 
 #==========================#
 # Main routine entry point #
@@ -57,6 +131,16 @@ if __name__ == '__main__':
 	print "****************************"
 	print "* Simulate CTA observation *"
 	print "****************************"
-	
+
+	# Set energy range in TeV
+	xmlname  = "data/source1.xml" # Source model
+	e_min    = 0.1                # 0.1 TeV
+	e_max    = 100.0              # 100 TeV
+	area     = 3200000.0 * 1.0e4  # 3200000.0 m^2
+	duration = 1800               # 30 min
+
     # Perform simulation
-	simulate("data/source1.xml")
+	photons = simulate(xmlname, e_min, e_max, area, duration)
+
+	# Show photons
+	show_photons(photons, xmlname, e_min, e_max, area, duration)
