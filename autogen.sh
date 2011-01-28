8c190f9db620152626517c3ab269cafe4f507f59
knodlseder <>
2011-01-28 22:14:28 +0000

    do not overwrite INSTALL file anymore

--- autogen.sh
+++ autogen.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
-autoreconf --force --install
+
+#autoreconf --force --install
 #autoreconf --force --install --symlink \
 #&& aclocal -I m4 \
 #&& automake --add-missing \
 #&& autoconf
-
+aclocal -I m4
+libtoolize --copy
+autoconf
+autoheader
+automake --add-missing --copy
 
