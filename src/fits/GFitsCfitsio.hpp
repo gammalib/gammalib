ce4b9dcc12fc82d88949165b73c6b5d75592a677
knodlseder <>
2011-02-10 23:35:54 +0000

    correct Doxygen documentation

--- src/fits/GFitsCfitsio.hpp
+++ src/fits/GFitsCfitsio.hpp
@@ -11,7 +11,7 @@
  *                                                                         *
  ***************************************************************************/
 /**
- * @file GFitsCfitsio.cpp
+ * @file GFitsCfitsio.hpp
  * @brief CFITSIO interface header
  * @author J. Knodlseder
  */
