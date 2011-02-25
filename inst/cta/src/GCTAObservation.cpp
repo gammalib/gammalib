290be23b13a9a208502e3031ac2a010128496c16
knodlseder <>
2011-02-25 11:29:36 +0000

    catch zero ontime in attribute writing

--- inst/cta/src/GCTAObservation.cpp
+++ inst/cta/src/GCTAObservation.cpp
@@ -4,10 +4,18 @@
  *  copyright (C) 2010-2011 by Jurgen Knodlseder                           *
  * ----------------------------------------------------------------------- *
  *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
+ *  This program is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
  *                                                                         *
  ***************************************************************************/
 /**
@@ -527,7 +535,7 @@ void GCTAObservation::write_attributes(GFitsHDU* hdu) const
         double tstop   = events()->tstop().met();
         double telapse = events()->gti().telapse();
         double ontime  = events()->gti().ontime();
-        double deadc   = livetime() / ontime;
+        double deadc   = (ontime > 0.0) ? livetime() / ontime : 0.0;
 
         // Set observation information
         hdu->card("CREATOR",  "GammaLib", "Program which created the file");
