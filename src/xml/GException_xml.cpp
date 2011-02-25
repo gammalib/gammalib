b49a58293bd9fd3d86c76867bed2bc5ef15bd51f
knodlseder <>
2011-02-25 14:20:27 +0000

    handles correctly empty segment argument in xml_syntax_error

--- src/xml/GException_xml.cpp
+++ src/xml/GException_xml.cpp
@@ -1,13 +1,21 @@
 /***************************************************************************
  *               GException_xml.cpp  -  XML exception handlers             *
  * ----------------------------------------------------------------------- *
- *  copyright (C) 2010 by Jurgen Knodlseder                                *
+ *  copyright (C) 2010-2011 by Jurgen Knodlseder                           *
  * ----------------------------------------------------------------------- *
  *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
+ *  This program is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
  *                                                                         *
  ***************************************************************************/
 
@@ -32,7 +40,13 @@ GException::xml_syntax_error::xml_syntax_error(std::string origin,
 {
     // Set origin and message
     m_origin  = origin;
-    m_message = "XML syntax error ("+message+") occured in "+segment;
+    
+    // Set message
+    m_message = "XML syntax error ("+message+")";
+
+    // Set segment
+    if (segment.length() > 0)
+        m_message += " occured in "+segment;
 
     // Return
     return;
