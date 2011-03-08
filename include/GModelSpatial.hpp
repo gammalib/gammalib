bc30f1fe4b7d90cf564aa119b63bce53081bd108
knodlseder <>
2011-03-08 00:46:04 +0000

    update documentation

--- include/GModelSpatial.hpp
+++ include/GModelSpatial.hpp
@@ -4,10 +4,18 @@
  *  copyright (C) 2009-2011 by Jurgen Knodlseder                           *
  * ----------------------------------------------------------------------- *
  *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
+ *  This program is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
  *                                                                         *
  ***************************************************************************/
 /**
@@ -36,9 +44,8 @@
  * @brief Abstract spatial model base class
  *
  * This class implements the spatial component of the factorized gamma-ray
- * source model. Typical examples of spatial components are a point source
- * (implemented by the derived class GModelSpatialPtsrc) or a Gaussian
- * source (implemented by the derived class GModelSpatialGauss).
+ * source model. A typical example of a spatial component is a point source
+ * (implemented by the derived class GModelSpatialPtsrc).
  ***************************************************************************/
 class GModelSpatial {
 
