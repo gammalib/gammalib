ef2e08f56314ab65e5b3f395b99244827bd0cdfa
knodlseder <>
2011-06-29 05:46:40 +0000

    add sincos() function

--- include/GTools.hpp
+++ include/GTools.hpp
@@ -4,10 +4,18 @@
  *  copyright (C) 2008-2011 by Jurgen Knodlseder                           *
  * ----------------------------------------------------------------------- *
  *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
+ *  This program is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
  *                                                                         *
  ***************************************************************************/
 /**
@@ -84,6 +92,7 @@ std::string              center(const std::string& s, int n, char c = ' ');
 std::string              parformat(const std::string& s);
 double                   modulo(double v1, double v2);
 double                   arccos(const double& arg);
+void                     sincos(const double& arg, double *sinval, double *cosval);
 bool                     file_exists(const std::string& filename);
 bool                     isinfinite(double x);
 
--- src/support/GTools.cpp
+++ src/support/GTools.cpp
@@ -4,10 +4,18 @@
  *  copyright (C) 2008-2011 by Jurgen Knodlseder                           *
  * ----------------------------------------------------------------------- *
  *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
+ *  This program is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
  *                                                                         *
  ***************************************************************************/
 /**
@@ -568,7 +576,7 @@ double modulo(double v1, double v2)
  *
  * @param[in] arg Argument.
  *
- * Returns the arcus cosinus by restricting the argument to [-1,1].
+ * Returns the arcus cosine by restricting the argument to [-1,1].
  ***************************************************************************/
 double arccos(const double& arg)
 {
@@ -585,6 +593,28 @@ double arccos(const double& arg)
 
     // Return result
     return arccos;
+}
+
+
+/***********************************************************************//**
+ * @brief Compute sine and cosine of argument
+ *
+ * @param[in] arg Argument.
+ * @param[out] sinval Sine value.
+ * @param[out] cosval Cosine value.
+ *
+ * Returns the sine and cosine of the specified value.
+ ***************************************************************************/
+void sincos(const double& arg, double *sinval, double *cosval)
+{
+    // Compute sine
+    *sinval = std::sin(arg);
+    
+    // Compute cosine
+    *cosval = std::sqrt(1.0 - *sinval * *sinval);
+    
+    // Return
+    return;
 }
 
 
