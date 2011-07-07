0b2bc559c43be6fbfcd02ed511f89099968fd7fe
knodlseder <>
2011-07-07 21:13:35 +0000

    add azimuth and zenith methods

--- inst/cta/include/GCTAPointing.hpp
+++ inst/cta/include/GCTAPointing.hpp
@@ -1,13 +1,21 @@
 /***************************************************************************
  *                 GCTAPointing.hpp  -  CTA pointing class                 *
  * ----------------------------------------------------------------------- *
- *  copyright (C) 2010 by Jurgen Knodlseder                                *
+ *  copyright (C) 2010-2011 by Jurgen Knodlseder                           *
  * ----------------------------------------------------------------------- *
  *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
+ *  This program is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
  *                                                                         *
  ***************************************************************************/
 /**
@@ -54,8 +62,10 @@ public:
     virtual std::string    print(void) const;
 
     // Other methods
-    void  dir(const GSkyDir& dir);
-    const GMatrix& rot(void) const;
+    void   dir(const GSkyDir& dir);
+    const  GMatrix& rot(void) const;
+    double zenith(void) const { return m_zenith; }
+    double azimuth(void) const { return m_azimuth; }
 
 protected:
     // Protected methods
@@ -65,7 +75,9 @@ protected:
     void update(void) const;
 
     // Protected members
-    GSkyDir         m_dir;        //!< Pointing direction
+    GSkyDir         m_dir;        //!< Pointing direction in sky coordinates
+    double          m_zenith;     //!< Pointing zenith angle
+    double          m_azimuth;    //!< Pointing azimuth angle
 
     // Cached members
     mutable bool    m_has_cache;  //!< Has transformation cache
--- inst/cta/src/GCTAPointing.cpp
+++ inst/cta/src/GCTAPointing.cpp
@@ -4,10 +4,18 @@
  *  copyright (C) 2010-2011 by Jurgen Knodlseder                           *
  * ----------------------------------------------------------------------- *
  *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
+ *  This program is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
  *                                                                         *
  ***************************************************************************/
 /**
@@ -232,6 +240,8 @@ void GCTAPointing::init_members(void)
 {
     // Initialise members
     m_dir.clear();
+    m_zenith    = 0.0;
+    m_azimuth   = 0.0;
     m_has_cache = false;
     m_Rback = GMatrix();
 
@@ -249,6 +259,8 @@ void GCTAPointing::copy_members(const GCTAPointing& pnt)
 {
     // Copy members
     m_dir       = pnt.m_dir;
+    m_zenith    = pnt.m_zenith;
+    m_azimuth   = pnt.m_azimuth;
     m_has_cache = pnt.m_has_cache;
     m_Rback     = pnt.m_Rback;
 
