f5c94a681a6faad8c7be7c0e2b6695607430a7b6
knodlseder <>
2011-07-08 19:30:34 +0000

    add parameter limits

--- src/model/GModelSpectralConst.cpp
+++ src/model/GModelSpectralConst.cpp
@@ -4,10 +4,18 @@
  *  copyright (C) 2009-2011 by Jurgen Knodlseder                           *
  * ----------------------------------------------------------------------- *
  *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
+ *  This program is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
  *                                                                         *
  ***************************************************************************/
 /**
@@ -407,12 +415,12 @@ std::string GModelSpectralConst::print(void) const
  ***************************************************************************/
 void GModelSpectralConst::init_members(void)
 {
-    // Initialise powerlaw normalisation
+    // Initialise constant normalisation
     m_norm.clear();
     m_norm.name("Value");
-    m_norm.value(1.0);
     m_norm.scale(1.0);
-    m_norm.range(0.0, 1000.0);
+    m_norm.value(1.0);         // default: 1.0
+    m_norm.range(0.0, 1000.0); // range:   [0, 1000]
     m_norm.free();
     m_norm.gradient(0.0);
     m_norm.hasgrad(true);
