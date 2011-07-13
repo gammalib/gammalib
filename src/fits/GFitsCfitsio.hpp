5d0f0d7494c2032cf0dbdb2ad03845ea788e3686
knodlseder <>
2011-07-13 09:04:28 +0000

    support cfitsio headers in cfitsio/fitsio.h

--- src/fits/GFitsCfitsio.hpp
+++ src/fits/GFitsCfitsio.hpp
@@ -4,10 +4,18 @@
  *  copyright (C) 2008-2011 by Jurgen Knodlseder                           *
  * ----------------------------------------------------------------------- *
  *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
+ *  This program is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
  *                                                                         *
  ***************************************************************************/
 /**
@@ -32,7 +40,13 @@
  *                           CFITSIO is available                          *
  ***************************************************************************/
 #if defined(HAVE_LIBCFITSIO)
+#if defined(HAVE_CFITSIO_FITSIO_H)
+#include <cfitsio/fitsio.h>
+#elif defined(HAVE_FITSIO_H)
 #include <fitsio.h>
+#else
+#include <fitsio.h>
+#endif
 
 /* __ Macros _____________________________________________________________ */
 #define __ffclos(A, B) ffclos(A, B)
