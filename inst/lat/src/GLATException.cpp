d27d410df5a8fc0d479fc84546bfe7f6d78f7958
knodlseder <>
2011-07-21 20:46:26 +0000

    add bad_response_type exception

--- inst/lat/src/GLATException.cpp
+++ inst/lat/src/GLATException.cpp
@@ -3,11 +3,18 @@
  * ----------------------------------------------------------------------- *
  *  copyright (C) 2010-2011 by Jurgen Knodlseder                           *
  * ----------------------------------------------------------------------- *
+ *  This program is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
  *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
  *                                                                         *
  ***************************************************************************/
 /**
@@ -261,5 +268,27 @@ GLATException::inconsistent_response::inconsistent_response(std::string origin,
     m_origin  = origin;
     m_message = "Inconsistent response table found. Expected "+str(expect)+
                 " elements, found "+str(size)+". " + message;
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Response is not a LAT response
+ *
+ * @param[in] origin Name of method that has thrown the exception.
+ * @param[in] message Optional error message.
+ ***************************************************************************/
+GLATException::bad_response_type::bad_response_type(std::string origin,
+                                                    std::string message)
+{
+    // Set origin
+    m_origin = origin;
+
+    // Set message
+    m_message = "Response is not of type GLATResponse.";
+    if (message.length() > 0)
+        m_message += " "+message;
+
+    // Return
     return;
 }
