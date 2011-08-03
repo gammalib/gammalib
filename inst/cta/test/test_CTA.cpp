8557c589f958b84ffbecbcfd17101129991cd860
knodlseder <>
2011-08-03 16:42:36 +0000

    adjust effective area sum to expected value

--- inst/cta/test/test_CTA.cpp
+++ inst/cta/test/test_CTA.cpp
@@ -4,10 +4,18 @@
  *  copyright (C) 2010-2011 by Jurgen Knodlseder                           *
  * ----------------------------------------------------------------------- *
  *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
+ *  This program is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
  *                                                                         *
  ***************************************************************************/
 /**
@@ -53,7 +61,7 @@ void test_response_aeff(void)
         // Sum over effective area for control
         GEnergy      eng;
         double sum = 0.0;
-        double ref = 123299125000.0;
+        double ref = 154124059000.00006; //!< Adjust to actual value
         for (int i = 0; i < 30; ++i) {
             eng.TeV(pow(10.0, -1.7 + 0.1*double(i)));
             double aeff = rsp.aeff(0.0, 0.0, 0.0, 0.0, eng.log10TeV());
