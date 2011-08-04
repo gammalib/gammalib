08b4792d56c5c2ca5d36bf86ddb4cdc742545d37
knodlseder <>
2011-08-04 21:46:15 +0000

    add remove_rows() method

--- include/GFitsTable.hpp
+++ include/GFitsTable.hpp
@@ -1,18 +1,26 @@
 /***************************************************************************
  *             GFitsTable.hpp  - FITS table abstract base class            *
  * ----------------------------------------------------------------------- *
- *  copyright (C) 2008-2010 by Jurgen Knodlseder                           *
+ *  copyright (C) 2008-2011 by Jurgen Knodlseder                           *
  * ----------------------------------------------------------------------- *
  *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
+ *  This program is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
  *                                                                         *
  ***************************************************************************/
 /**
  * @file GFitsTable.hpp
- * @brief GFitsTable class definition.
+ * @brief FITS table abstract base class interface definition
  * @author J. Knodlseder
  */
 
@@ -61,6 +69,7 @@ public:
     void           insert_column(int colnum, GFitsTableCol& column);
     void           append_rows(const int& nrows);
     void           insert_rows(const int& rownum, const int& nrows);
+    void           remove_rows(const int& rownum, const int& nrows);
     GFitsTableCol* column(const std::string& colname);
     GFitsTableCol* column(const int& colnum);
     int            nrows(void) const;
