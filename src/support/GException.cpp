95ea49dc21cbc6613991c53b4d5282ad7e2bf05f
knodlseder <>
2011-08-05 06:50:45 +0000

    update

--- src/support/GException.cpp
+++ src/support/GException.cpp
@@ -4,10 +4,18 @@
  *  copyright (C) 2006-2011 by Jurgen Knodlseder                           *
  * ----------------------------------------------------------------------- *
  *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
+ *  This program is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
  *                                                                         *
  ***************************************************************************/
 /**
@@ -88,7 +96,7 @@ GException::invalid_argument::invalid_argument(std::string origin,
 GException::bad_type::bad_type(std::string origin, std::string message)
 {
     m_origin   = origin;
-    m_message = "Invalud type conversion. " + message;
+    m_message = "Invalid type conversion. " + message;
     return;
 }
 
