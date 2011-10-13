d2b5fe896bc2f7e06fe3f29519fc7455893ead5f
knodlseder <>
2011-10-13 06:18:12 +0000

    update license information

--- inst/cta/src/GCTAModelRadialAcceptance.cpp
+++ inst/cta/src/GCTAModelRadialAcceptance.cpp
@@ -4,10 +4,18 @@
  *  copyright (C) 2011 by Jurgen Knodlseder                                *
  * ----------------------------------------------------------------------- *
  *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
+ *  This program is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
  *                                                                         *
  ***************************************************************************/
 /**
@@ -60,7 +68,7 @@ const GModelRegistry            g_cta_radial_acceptance_registry(&g_cta_radial_a
 /* __ Coding definitions _________________________________________________ */
 
 /* __ Debug definitions __________________________________________________ */
-#define G_DUMP_MC 0                                 //!< Dump MC information
+//#define G_DUMP_MC                                  //!< Dump MC information
 
 
 /*==========================================================================
@@ -464,7 +472,7 @@ GCTAEventList* GCTAModelRadialAcceptance::mc(const GObservation& obs,
             double rate = flux * area;
 
             // Debug option: dump rate
-            #if G_DUMP_MC
+            #if defined(G_DUMP_MC)
             std::cout << "GCTAModelRadialAcceptance::mc(\"" << name() << "\": ";
             std::cout << "flux=" << flux << " cts/s/sr, ";
             std::cout << "area=" << area << " sr, ";
