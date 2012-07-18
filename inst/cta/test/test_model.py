7f0e30dba1963d1161e5b8de3ab004117bd5a102
Jürgen Knödlseder <jurgen.knodlseder@irap.omp.eu>
2012-07-18 14:28:13 +0200

    Update code for new interface and add testing of diffuse source model.

--- inst/cta/test/test_model.py
+++ inst/cta/test/test_model.py
@@ -1,5 +1,23 @@
 #! /usr/bin/env python
-
+# ==========================================================================
+# This script tests the model computations using the CTA response function.
+#
+# Copyright (C) 2011-2012 Jurgen Knodlseder
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# ==========================================================================
 from gammalib import *
 
 
@@ -99,10 +117,32 @@ def gauss_model(ra=359.6, dec=+0.1, sigma=0.2):
 	return model
 
 
+# ================= #
+# Set diffuse model #
+# ================= #
+def diffuse_model(file="../../../test/data/cena_lobes_parkes.fits"):
+	"""
+	Set Diffuse model.
+	"""
+	# Set spatial model
+	spatial = GModelSpatialMap(file)
+
+	# Set spectral model
+	spectral = GModelSpectralPlaw(1.0, -2.0)
+
+	# Set sky model
+	model = GModelDiffuseSource(spatial, spectral)
+
+	# Return model
+	return model
+
+
 # ========================== #
 # Set binned CTA observation #
 # ========================== #
-def observation(ra=0.0, dec=0.0, binsz=0.02, npix=100, ebins=10):
+def observation(ra=0.0, dec=0.0, emin=0.1, emax=100.0,
+                binsz=0.02, npix=100, ebins=10,
+                duration=1800.0, deadc=0.95):
 	"""
 	Set binned CTA observation.
 	"""
@@ -121,20 +161,25 @@ def observation(ra=0.0, dec=0.0, binsz=0.02, npix=100, ebins=10):
 	
 	# Set
 	ebounds = GEbounds()
-	emin    = GEnergy()
-	emax    = GEnergy()
-	emin.TeV(0.1)
-	emax.TeV(100.0)
-	ebounds.setlog(emin, emax, ebins)
+	e_min   = GEnergy()
+	e_max   = GEnergy()
+	e_min.TeV(emin)
+	e_max.TeV(emax)
+	ebounds.setlog(e_min, e_max, ebins)
 	gti     = GGti()
 	tmin    = GTime()
 	tmax    = GTime()
 	tmin.met(0.0)
-	tmax.met(1800.0)
+	tmax.met(duration)
 	gti.append(tmin, tmax)
 	map     = GSkymap("CAR", "CEL", ra, dec, -binsz, binsz, npix, npix, ebins)
 	cube    = GCTAEventCube(map, ebounds, gti)
 	obs.events(cube)
+
+	# Set ontime, livetime, and deadtime correction factor
+	obs.ontime(duration)
+	obs.livetime(duration*deadc)
+	obs.deadc(deadc)
 	
 	# Optionally show observation
 	#print obs
@@ -146,14 +191,17 @@ def observation(ra=0.0, dec=0.0, binsz=0.02, npix=100, ebins=10):
 # ==================== #
 # Test IRF computation #
 # ==================== #
-def test_irf(model, filename="cntmap.fits"):
+def test_irf(model, ra=0.0, dec=0.0, npix=100, filename="cntmap.fits"):
 	"""
 	Test IRF.
 	"""
+	# Print model
+	print model
+
 	# Set CTA observation
-	obs = observation()
+	obs = observation(ra=ra, dec=dec, npix=npix)
 	src = GSkyDir()
-	src.radec_deg(0.0,0.0)
+	src.radec_deg(ra,dec)
 	
 	# Loop over all bins
 	for bin in obs.events():
@@ -182,14 +230,17 @@ def test_irf(model, filename="cntmap.fits"):
 # ========================= #
 # Test gradient computation #
 # ========================= #
-def test_grad(model, filename="gradmap.fits", ipar=2):
+def test_grad(model, ra=0.0, dec=0.0, filename="gradmap.fits", ipar=2):
 	"""
 	Test gradient computation.
 	"""
+	# Print model
+	print model
+
 	# Set CTA observation
 	obs = observation()
 	src = GSkyDir()
-	src.radec_deg(0.0,0.0)
+	src.radec_deg(ra,dec)
 	
 	# Make sure that parameter is free
 	model[ipar].free()
@@ -230,16 +281,17 @@ if __name__ == '__main__':
 	print "* Test diffuse models *"
 	print "***********************"
 	
-	# Set shell model
-	#model = ptsrc_model()
-	model = shell_model()
-	#model = disk_model()
-	#model = gauss_model()
-	
-	# Print model
-	print model
-	
 	# Test IRF
-	#test_irf(model)
-	test_grad(model, ipar=3)
+	#test_irf(ptsrc_model())
+	#test_irf(shell_model())
+	#test_irf(disk_model())
+	#test_irf(gauss_model())
+	test_irf(diffuse_model(), ra=201.3651, dec=-43.0191, npix=200)
+
+	# Test Gradient
+	#test_grad(ptsrc_model(), ipar=3)
+	#test_grad(shell_model(), ipar=3)
+	#test_grad(disk_model(), ipar=3)
+	#test_grad(gauss_model(), ipar=3)
+	#test_grad(diffuse_model(), ra=201.3651, dec=-43.0191, ipar=3)
 
