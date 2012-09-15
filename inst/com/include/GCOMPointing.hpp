15a7cd5729418a43e8e1c89d23491d8d457f167b
Jürgen Knödlseder <jurgen.knodlseder@irap.omp.eu>
2012-09-15 21:22:20 +0200

    Initial version.
    The COMPTEL pointing consists of a simple sky direction. No coordinate transformations are so far performed.

---
+++ inst/com/include/GCOMPointing.hpp
@@ -0,0 +1,75 @@
+/***************************************************************************
+ *               GCOMPointing.hpp  -  COMPTEL pointing class               *
+ * ----------------------------------------------------------------------- *
+ *  copyright (C) 2012 by Juergen Knoedlseder                              *
+ * ----------------------------------------------------------------------- *
+ *                                                                         *
+ *  This program is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ *                                                                         *
+ ***************************************************************************/
+/**
+ * @file GCOMPointing.hpp
+ * @brief COMPTEL pointing class interface definition
+ * @author Juergen Knoedlseder
+ */
+
+#ifndef GCOMPOINTING_HPP
+#define GCOMPOINTING_HPP
+
+/* __ Includes ___________________________________________________________ */
+#include <string>
+#include "GPointing.hpp"
+#include "GSkyDir.hpp"
+
+
+/***********************************************************************//**
+ * @class GCOMPointing
+ *
+ * @brief Interface for the COMPTEL pointing class.
+ *
+ * This class implements a COMPTEL pointing. For the time being it is assumed
+ * that the pointing direction is time-independent.
+ ***************************************************************************/
+class GCOMPointing : public GPointing {
+
+public:
+    // Constructors and destructors
+    GCOMPointing(void);
+    explicit GCOMPointing(const GSkyDir& dir);
+    GCOMPointing(const GCOMPointing& pnt);
+    virtual ~GCOMPointing(void);
+
+    // Operators
+    virtual GCOMPointing& operator=(const GCOMPointing& pnt);
+
+    // Implemented pure virtual methods
+    virtual void           clear(void);
+    virtual GCOMPointing*  clone(void) const;
+    virtual const GSkyDir& dir(void) const;
+    virtual std::string    print(void) const;
+
+    // Other methods
+    void dir(const GSkyDir& dir);
+
+protected:
+    // Protected methods
+    void init_members(void);
+    void copy_members(const GCOMPointing& pnt);
+    void free_members(void);
+
+    // Protected members
+    GSkyDir m_dir;    //!< Pointing direction in sky coordinates
+};
+
+#endif /* GCOMPOINTING_HPP */
---
+++ inst/com/src/GCOMPointing.cpp
@@ -0,0 +1,276 @@
+/***************************************************************************
+ *               GCOMPointing.cpp  -  COMPTEL pointing class               *
+ * ----------------------------------------------------------------------- *
+ *  copyright (C) 2012 by Juergen Knoedlseder                              *
+ * ----------------------------------------------------------------------- *
+ *                                                                         *
+ *  This program is free software: you can redistribute it and/or modify   *
+ *  it under the terms of the GNU General Public License as published by   *
+ *  the Free Software Foundation, either version 3 of the License, or      *
+ *  (at your option) any later version.                                    *
+ *                                                                         *
+ *  This program is distributed in the hope that it will be useful,        *
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of         *
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          *
+ *  GNU General Public License for more details.                           *
+ *                                                                         *
+ *  You should have received a copy of the GNU General Public License      *
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
+ *                                                                         *
+ ***************************************************************************/
+/**
+ * @file GCOMPointing.cpp
+ * @brief COMPTEL pointing class interface implementation
+ * @author Juergen Knoedlseder
+ */
+
+/* __ Includes ___________________________________________________________ */
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+#include "GTools.hpp"
+#include "GCOMPointing.hpp"
+
+/* __ Method name definitions ____________________________________________ */
+
+/* __ Macros _____________________________________________________________ */
+
+/* __ Coding definitions _________________________________________________ */
+
+/* __ Debug definitions __________________________________________________ */
+
+
+
+/*==========================================================================
+ =                                                                         =
+ =                         Constructors/destructors                        =
+ =                                                                         =
+ ==========================================================================*/
+
+/***********************************************************************//**
+ * @brief Void constructor
+ ***************************************************************************/
+GCOMPointing::GCOMPointing(void) : GPointing()
+{
+    // Initialise members
+    init_members();
+
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Sky direction constructor
+ *
+ * @param[in] dir Sky direction.
+ *
+ * Construct COMPTEL pointing from sky direction.
+ ***************************************************************************/
+GCOMPointing::GCOMPointing(const GSkyDir& dir) : GPointing()
+{
+    // Initialise members
+    init_members();
+
+    // Assign sky direction
+    this->dir(dir);
+
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Copy constructor
+ *
+ * @param[in] pnt COMPTEL pointing.
+ ***************************************************************************/
+GCOMPointing::GCOMPointing(const GCOMPointing& pnt) : GPointing(pnt)
+{
+    // Initialise members
+    init_members();
+
+    // Copy members
+    copy_members(pnt);
+
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Destructor
+ ***************************************************************************/
+GCOMPointing::~GCOMPointing(void)
+{
+    // Free members
+    free_members();
+
+    // Return
+    return;
+}
+
+
+/*==========================================================================
+ =                                                                         =
+ =                               Operators                                 =
+ =                                                                         =
+ ==========================================================================*/
+
+/***********************************************************************//**
+ * @brief Assignment operator
+ *
+ * @param[in] pnt COMPTEL pointing.
+ ***************************************************************************/
+GCOMPointing& GCOMPointing::operator= (const GCOMPointing& pnt)
+{
+    // Execute only if object is not identical
+    if (this != &pnt) {
+
+        // Copy base class members
+        this->GPointing::operator=(pnt);
+
+        // Free members
+        free_members();
+
+        // Initialise members
+        init_members();
+
+        // Copy members
+        copy_members(pnt);
+
+    } // endif: object was not identical
+
+    // Return this object
+    return *this;
+}
+
+
+/*==========================================================================
+ =                                                                         =
+ =                             Public methods                              =
+ =                                                                         =
+ ==========================================================================*/
+
+/***********************************************************************//**
+ * @brief Clear instance
+ ***************************************************************************/
+void GCOMPointing::clear(void)
+{
+    // Free members
+    free_members();
+    this->GPointing::free_members();
+
+    // Initialise private members
+    this->GPointing::init_members();
+    init_members();
+
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Clone instance
+ ***************************************************************************/
+GCOMPointing* GCOMPointing::clone(void) const
+{
+    return new GCOMPointing(*this);
+}
+
+
+/***********************************************************************//**
+ * @brief Return pointing direction
+ *
+ * Returns reference to sky direction of the pointing.
+ ***************************************************************************/
+const GSkyDir& GCOMPointing::dir(void) const
+{
+    // Return
+    return m_dir;
+}
+
+
+/***********************************************************************//**
+ * @brief Set pointing direction
+ *
+ * @param[in] dir Sky direction.
+ *
+ * Sets the sky direction for the pointing.
+ ***************************************************************************/
+void GCOMPointing::dir(const GSkyDir& dir)
+{
+    // Set sky direction
+    m_dir = dir;
+
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Print COMPTEL pointing information
+ ***************************************************************************/
+std::string GCOMPointing::print(void) const
+{
+    // Initialise result string
+    std::string result;
+
+    // Append header
+    result.append("=== GCOMPointing ===");
+    result.append("\n"+parformat("Pointing direction")+this->dir().print());
+
+    // Return result
+    return result;
+}
+
+
+/*==========================================================================
+ =                                                                         =
+ =                             Private methods                             =
+ =                                                                         =
+ ==========================================================================*/
+
+/***********************************************************************//**
+ * @brief Initialise class members
+ ***************************************************************************/
+void GCOMPointing::init_members(void)
+{
+    // Initialise members
+    m_dir.clear();
+
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Copy class members
+ *
+ * @param[in] pnt COM pointing.
+ ***************************************************************************/
+void GCOMPointing::copy_members(const GCOMPointing& pnt)
+{
+    // Copy members
+    m_dir = pnt.m_dir;
+
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
+ * @brief Delete class members
+ ***************************************************************************/
+void GCOMPointing::free_members(void)
+{
+    // Return
+    return;
+}
+
+
+/*==========================================================================
+ =                                                                         =
+ =                                 Friends                                 =
+ =                                                                         =
+ ==========================================================================*/
