b2343fd5617ad526fa49db704045134b058c8ea2
Jürgen Knödlseder <jurgen.knodlseder@irap.omp.eu>
2013-04-12 23:10:47 +0200

    Move mathematical constants from GTools.hpp to GMath.hpp.

--- include/GTools.hpp
+++ include/GTools.hpp
@@ -35,28 +35,10 @@
 
 /* __ Constants __________________________________________________________ */
 namespace gammalib {
-    const double pi           =  3.141592653589793238462643383279502884197;
-    const double twopi        =  6.283185307179586476925286766559005768394;
-    const double fourpi       = 12.56637061435917295385057353311801153679;
-    const double pihalf       =  1.570796326794896619231321691639751442099;
-    const double inv_pihalf   =  0.6366197723675813430755350534900574;
-    const double inv_sqrt4pi  =  0.2820947917738781434740397257803862929220;
-    const double pi2          = pi*pi;
-    const double deg2rad      =  0.0174532925199432954743717;
-    const double rad2deg      = 57.295779513082322864647722;
-    const double ln2          =  0.6931471805599453094172321214581766;
-    const double ln10         =  2.3025850929940456840179914546843642;
-    const double inv_ln2      =  1.4426950408889634073599246810018921;
-    const double onethird     =  1.0/3.0;
-    const double twothird     =  2.0/3.0;
-    const double fourthird    =  4.0/3.0;
-    const double sqrt_onehalf = std::sqrt(1.0/2.0);
-    const double sqrt_pihalf  = std::sqrt(pihalf);
-    const double sqrt_two     = std::sqrt(2.0);
-    const double MeV2erg      =  1.6021765e-6;
-    const double erg2MeV      =  624150.96;
-    const double pc2cm        =  3.08568025e18;
-    const double sec_in_day   = 86400.0;
+    const double MeV2erg    =  1.6021765e-6;
+    const double erg2MeV    =  624150.96;
+    const double pc2cm      =  3.08568025e18;
+    const double sec_in_day = 86400.0;
 }
 
 /* __ Prototypes ________________________________________________________ */
