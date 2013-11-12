9a86ee51f9a3b25c3d35dcc5d9da9c5bdccfd18a
Jürgen Knödlseder <jurgen.knodlseder@irap.omp.eu>
2013-11-12 21:52:52 +0100

    Properly allocate and free memory to prevent memory leaks.

--- test/testinst/GTestModelData.hpp
+++ test/testinst/GTestModelData.hpp
@@ -56,13 +56,12 @@ public:
     }
     explicit GTestModelData(const GXmlElement& xml) : GModelData(xml) {
         init_members();
-        //m_modelTps = new GModelTemporalConst();
         set_pointers();
         return;
     }
-        GTestModelData(const GTestModelData& model) : GModelData(model) { 
-        init_members();
+    GTestModelData(const GTestModelData& model) : GModelData(model) { 
         copy_members(model);
+        set_pointers();
         return;        
     }
     virtual ~GTestModelData(void) {
@@ -75,8 +74,8 @@ public:
         if (this != &model) {
             this->GModelData::operator=(model);
             free_members();
-            init_members();
             copy_members(model);
+            set_pointers();
         }
         return *this;
     }
@@ -229,27 +228,20 @@ protected:
     }
     void copy_members(const GTestModelData& model){
         m_modelTps = model.temporal()->clone();
-
-        // Set parameter pointers
-        set_pointers();
     }
     void free_members(void) {
         delete m_modelTps;
     }
-        
     void set_pointers(void){
-        //Clear parameters list
         m_pars.clear();
-        
-            for(int i=0;i<m_modelTps->size();i++){
-                (*m_modelTps)[i].free(); // Set free
-                m_pars.push_back(&((*m_modelTps)[i]));
-
-             }
+        for(int i = 0; i < m_modelTps->size(); ++i) {
+            (*m_modelTps)[i].free(); // Set free
+            m_pars.push_back(&((*m_modelTps)[i]));
+        }
     }
-    
-    GModelTemporalConst* m_modelTps; //!< Temporal model
 
+    // Protected members
+    GModelTemporalConst* m_modelTps; //!< Temporal model
 };
 
 #endif /* GMODELDATA_HPP */
