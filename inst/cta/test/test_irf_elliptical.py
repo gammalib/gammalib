8945de38f330c9c0723e2a42aed0a30e939cc346
Jürgen Knödlseder <jurgen.knodlseder@irap.omp.eu>
2014-10-28 16:24:48 +0100

    Implement new variable naming

--- inst/cta/test/test_irf_elliptical.py
+++ inst/cta/test/test_irf_elliptical.py
@@ -82,7 +82,7 @@ def cta_roi_arclength(rad, dist, roi):
 # ======================= #
 # Show ellptical response #
 # ======================= #
-def show_response(centre, semimajor, semiminor, posang, obsDir, pntDir, rhos):
+def show_response(centre, semimajor, semiminor, posang, obsDir, pntDir, delta_max, rhos, log=False):
     """
     Show ellptical response
     """
@@ -90,55 +90,39 @@ def show_response(centre, semimajor, semiminor, posang, obsDir, pntDir, rhos):
     plt.figure(1)
     plt.title("Elliptical response")
 
-    # Set some constants
-    delta_max = 0.5 * gammalib.deg2rad
-
-    # Put posang in [-pi,pi]
-    #if (posang > gammalib.pi):
-    #    posang -= gammalib.pi
-    #elif (posang < -gammalib.pi):
-    #    posang += gammalib.pi
+    # Convert delta_max to radians
+    delta_max *= gammalib.deg2rad
 
     # Compute some stuff
-    cos_pa   = math.cos(posang * gammalib.deg2rad)
-    sin_pa   = math.sin(posang * gammalib.deg2rad)
-    zeta     = centre.dist(obsDir)
-    obsOmega = centre.posang(obsDir)
-    eta      = pntDir.dist(obsDir)
-    lamda    = centre.dist(pntDir)
-    src_max  = semimajor * gammalib.deg2rad
-
-    # Omega_0
-    omega0 = 0.0
-    denom  = math.sin(lamda*gammalib.deg2rad) * math.sin(zeta*gammalib.deg2rad)
-    if (denom != 0.0):
-        arg    = (math.cos(eta*gammalib.deg2rad) - math.cos(lamda*gammalib.deg2rad) * \
-                  math.cos(zeta*gammalib.deg2rad))/denom;
-        omega0 = gammalib.acos(arg)
-    pa_pnt = centre.posang(pntDir)
+    cos_pa       = math.cos(posang * gammalib.deg2rad)
+    sin_pa       = math.sin(posang * gammalib.deg2rad)
+    rho_obs      = centre.dist(obsDir)
+    posangle_obs = centre.posang(obsDir)
+    rho_pnt      = centre.dist(pntDir)
+    posangle_pnt = centre.posang(pntDir)
+    omega_pnt    = posangle_pnt - posangle_obs
+    src_max      = semimajor * gammalib.deg2rad
 
     # Rho limits
     rho_min = 0.0
-    rho_max = zeta + delta_max
-    if (zeta > delta_max):
-        rho_min = zeta - delta_max
+    rho_max = rho_obs + delta_max
+    if (rho_obs > delta_max):
+        rho_min = rho_obs - delta_max
     if rho_max > src_max:
         rho_max = src_max
 
     # Setup ellipse as vector
-    ellipse_x = []
-    ellipse_y = []
+    x = []
+    y = []
     for angle in range(0, 360):
         radians     = angle * gammalib.deg2rad
         cos_radians = math.cos(radians)
         sin_radians = math.sin(radians)
-        x = semimajor * cos_radians * sin_pa - semiminor * sin_radians * cos_pa
-        y = semimajor * cos_radians * cos_pa + semiminor * sin_radians * sin_pa
-        ellipse_x.append(x)
-        ellipse_y.append(y)
+        x.append(semimajor * cos_radians * sin_pa - semiminor * sin_radians * cos_pa)
+        y.append(semimajor * cos_radians * cos_pa + semiminor * sin_radians * sin_pa)
 
     # Plot ellipse
-    plt.plot(ellipse_x, ellipse_y, 'r-')
+    plt.plot(x, y, 'r-')
     plt.xlim([ 1.5*semimajor,-1.5*semimajor])
     plt.ylim([-1.5*semimajor, 1.5*semimajor])
     plt.plot([0.0], [0.0], 'ro')
@@ -150,26 +134,24 @@ def show_response(centre, semimajor, semiminor, posang, obsDir, pntDir, rhos):
 
     # Draw connecting line model-photon'
     n    = 10
-    step = zeta/n * gammalib.rad2deg
+    step = rho_obs/n * gammalib.rad2deg
     x    = []
     y    = []
     for i in range(n+1):
-        x.append(i*step * math.sin(obsOmega))
-        y.append(i*step * math.cos(obsOmega))
+        x.append(i*step * math.sin(posangle_obs))
+        y.append(i*step * math.cos(posangle_obs))
     plt.plot(x, y, 'b-')
 
     # Plot Psf validity circle
-    circle_x = []
-    circle_y = []
+    x = []
+    y = []
     for angle in range(0, 360):
         radians     = angle * gammalib.deg2rad
         cos_radians = math.cos(radians)
         sin_radians = math.sin(radians)
-        x = delta_max * cos_radians * gammalib.rad2deg + x_obs
-        y = delta_max * sin_radians * gammalib.rad2deg + y_obs
-        circle_x.append(x)
-        circle_y.append(y)
-    plt.plot(circle_x, circle_y, 'b-')
+        x.append(delta_max * cos_radians * gammalib.rad2deg + x_obs)
+        y.append(delta_max * sin_radians * gammalib.rad2deg + y_obs)
+    plt.plot(x, y, 'b-')
 
     # Plot pointing direction
     x = pntDir.ra_deg()  - centre.ra_deg()
@@ -177,28 +159,29 @@ def show_response(centre, semimajor, semiminor, posang, obsDir, pntDir, rhos):
     plt.plot([x], [y], 'go')
 
     # Print some stuff
-    print("Distance p'-model (zeta) .............: %.2f deg" % (zeta*gammalib.rad2deg))
-    print("PA of p' from model (obsOmega)........: %.2f deg" % (obsOmega*gammalib.rad2deg))
-    print("Distance p'-pointing (eta)............: %.2f deg" % (eta*gammalib.rad2deg))
-    print("Distance model-pointing (lambda) .....: %.2f deg" % (lamda*gammalib.rad2deg))
-    print("Omega_0 ..............................: %.2f deg" % (omega0*gammalib.rad2deg))
-    print("PA of pointing from model ............: %.2f deg" % (pa_pnt*gammalib.rad2deg))
-    print("Rho interval .........................: %.2f-%.2f deg" % (rho_min*gammalib.rad2deg,rho_max*gammalib.rad2deg))
+    print("Distance model-event (rho_obs) ..........: %.2f deg" % (rho_obs*gammalib.rad2deg))
+    print("PA of event from model (posangle_obs) ...: %.2f deg" % (posangle_obs*gammalib.rad2deg))
+    print("Distance model-pointing (rho_pnt) .......: %.2f deg" % (rho_pnt*gammalib.rad2deg))
+    print("PA of pointing from model (posangle_pnt) : %.2f deg" % (posangle_pnt*gammalib.rad2deg))
+    print("Azimuth of pointing (omega_pnt) .........: %.2f deg" % (omega_pnt*gammalib.rad2deg))
+    print("Rho interval ............................: %.2f-%.2f deg" % (rho_min*gammalib.rad2deg,rho_max*gammalib.rad2deg))
 
     # Plot rhos
     for rho in rhos:
 
         # Print header
-        print("Rho ..................................: %.2f deg" % (rho))
+        if log:
+            print("Rho ..................................: %.2f deg" % (rho))
 
         # Compute some stuff
-        rho_rad   = rho   * gammalib.deg2rad
+        rho_rad = rho   * gammalib.deg2rad
 
         # Compute half length of arc that lies within Psf validity circle
-        domega = 0.5 * cta_roi_arclength(rho_rad, zeta, delta_max)
+        domega = 0.5 * cta_roi_arclength(rho_rad, rho_obs, delta_max)
 
         # Print domega
-        print("  domega .............................: %.2f deg" % (domega*gammalib.rad2deg))
+        if log:
+            print("  domega .............................: %.2f deg" % (domega*gammalib.rad2deg))
 
         # Continue only if arc length is positive
         if (domega > 0.0):
@@ -206,10 +189,10 @@ def show_response(centre, semimajor, semiminor, posang, obsDir, pntDir, rhos):
             # Precompute cosine and sine terms for azimuthal integration
             cos_rho = math.cos(rho_rad);
             sin_rho = math.sin(rho_rad);
-            cos_psf = cos_rho * math.cos(zeta)
-            sin_psf = sin_rho * math.sin(zeta)
-            cos_ph  = cos_rho * math.cos(lamda)
-            sin_ph  = sin_rho * math.sin(lamda)
+            cos_psf = cos_rho * math.cos(rho_obs)
+            sin_psf = sin_rho * math.sin(rho_obs)
+            cos_ph  = cos_rho * math.cos(rho_pnt)
+            sin_ph  = sin_rho * math.sin(rho_pnt)
 
             # Full containment within ellipse
             if (rho < semiminor):
@@ -219,7 +202,8 @@ def show_response(centre, semimajor, semiminor, posang, obsDir, pntDir, rhos):
                 omega_max = +domega
 
                 # Print
-                print("  Omega range ........................: %.2f - %.2f deg" % (omega_min*gammalib.rad2deg,omega_max*gammalib.rad2deg))
+                if log:
+                    print("  Omega range ........................: %.2f - %.2f deg" % (omega_min*gammalib.rad2deg,omega_max*gammalib.rad2deg))
 
                 # Plot segment
                 n    = 100
@@ -228,8 +212,8 @@ def show_response(centre, semimajor, semiminor, posang, obsDir, pntDir, rhos):
                 step = (omega_max - omega_min)/n
                 for i in range(n+1):
                     radians = i*step + omega_min
-                    cos_radians = math.sin(radians+obsOmega)
-                    sin_radians = math.cos(radians+obsOmega)
+                    cos_radians = math.sin(radians+posangle_obs)
+                    sin_radians = math.cos(radians+posangle_obs)
                     x.append(rho * cos_radians)
                     y.append(rho * sin_radians)
                 plt.plot(x, y, 'k-')
@@ -244,7 +228,8 @@ def show_response(centre, semimajor, semiminor, posang, obsDir, pntDir, rhos):
                 omega_width = math.acos(math.sqrt(arg1/arg2))
 
                 # Print Omega width
-                print("  omega_width ........................: %.2f deg" % (omega_width*gammalib.rad2deg))
+                if log:
+                    print("  omega_width ........................: %.2f deg" % (omega_width*gammalib.rad2deg))
 
                 # 
                 if (omega_width > 0.0):
@@ -253,7 +238,7 @@ def show_response(centre, semimajor, semiminor, posang, obsDir, pntDir, rhos):
                     # to connecting line between model and observed photon. Make
                     # sure that omega_0 is within [-pi,pi] thus that the omega
                     # intervals are between [-2pi,2pi]
-                    omega_0 = posang * gammalib.deg2rad - obsOmega
+                    omega_0 = posang * gammalib.deg2rad - posangle_obs
                     if (omega_0 > gammalib.pi):
                         omega_0 -= gammalib.pi
                     elif (omega_0 < -gammalib.pi):
@@ -265,8 +250,10 @@ def show_response(centre, semimajor, semiminor, posang, obsDir, pntDir, rhos):
                     omega2_min = omega1_min + gammalib.pi
                     omega2_max = omega1_max + gammalib.pi
 
-                    print("  Omega range 1 (initial) ............: %.2f - %.2f deg" % (omega1_min*gammalib.rad2deg,omega1_max*gammalib.rad2deg))
-                    print("  Omega range 2 (initial) ............: %.2f - %.2f deg" % (omega2_min*gammalib.rad2deg,omega2_max*gammalib.rad2deg))
+                    # Logging
+                    if log:
+                        print("  Omega range 1 (initial) ............: %.2f - %.2f deg" % (omega1_min*gammalib.rad2deg,omega1_max*gammalib.rad2deg))
+                        print("  Omega range 2 (initial) ............: %.2f - %.2f deg" % (omega2_min*gammalib.rad2deg,omega2_max*gammalib.rad2deg))
 
                     # Limit each of the intervals to [omega_min, omega_max]
                     omega_min       = -domega
@@ -317,10 +304,11 @@ def show_response(centre, semimajor, semiminor, posang, obsDir, pntDir, rhos):
                         omega2_max = 0.0
 
                     # Print results
-                    print("  omega_0 ............................: %.2f deg" % (omega_0*gammalib.rad2deg))
-                    print("  Omega range ........................: %.2f - %.2f deg" % (omega_min*gammalib.rad2deg,omega_max*gammalib.rad2deg))
-                    print("  Omega range 1 ......................: %.2f - %.2f deg" % (omega1_min*gammalib.rad2deg,omega1_max*gammalib.rad2deg))
-                    print("  Omega range 2 ......................: %.2f - %.2f deg" % (omega2_min*gammalib.rad2deg,omega2_max*gammalib.rad2deg))
+                    if log:
+                        print("  omega_0 ............................: %.2f deg" % (omega_0*gammalib.rad2deg))
+                        print("  Omega range ........................: %.2f - %.2f deg" % (omega_min*gammalib.rad2deg,omega_max*gammalib.rad2deg))
+                        print("  Omega range 1 ......................: %.2f - %.2f deg" % (omega1_min*gammalib.rad2deg,omega1_max*gammalib.rad2deg))
+                        print("  Omega range 2 ......................: %.2f - %.2f deg" % (omega2_min*gammalib.rad2deg,omega2_max*gammalib.rad2deg))
 
                     # Draw Omega range lines and circle segments
                     if (omega1_min < omega1_max):
@@ -329,14 +317,14 @@ def show_response(centre, semimajor, semiminor, posang, obsDir, pntDir, rhos):
                         #x    = []
                         #y    = []
                         #for i in range(n+1):
-                        #    x.append(i*step * math.sin(omega1_min+obsOmega))
-                        #    y.append(i*step * math.cos(omega1_min+obsOmega))
+                        #    x.append(i*step * math.sin(omega1_min+posangle_obs))
+                        #    y.append(i*step * math.cos(omega1_min+posangle_obs))
                         #plt.plot(x, y, 'k-.')
                         #x    = []
                         #y    = []
                         #for i in range(n+1):
-                        #    x.append(i*step * math.sin(omega1_max+obsOmega))
-                        #    y.append(i*step * math.cos(omega1_max+obsOmega))
+                        #    x.append(i*step * math.sin(omega1_max+posangle_obs))
+                        #    y.append(i*step * math.cos(omega1_max+posangle_obs))
                         #plt.plot(x, y, 'k-.')
                         n    = 100
                         x    = []
@@ -344,8 +332,8 @@ def show_response(centre, semimajor, semiminor, posang, obsDir, pntDir, rhos):
                         step = (omega1_max - omega1_min)/n
                         for i in range(n+1):
                             radians = i*step + omega1_min
-                            cos_radians = math.sin(radians+obsOmega)
-                            sin_radians = math.cos(radians+obsOmega)
+                            cos_radians = math.sin(radians+posangle_obs)
+                            sin_radians = math.cos(radians+posangle_obs)
                             x.append(rho * cos_radians)
                             y.append(rho * sin_radians)
                         plt.plot(x, y, 'k-')
@@ -356,14 +344,14 @@ def show_response(centre, semimajor, semiminor, posang, obsDir, pntDir, rhos):
                         #x    = []
                         #y    = []
                         #for i in range(n+1):
-                        #    x.append(i*step * math.sin(omega2_min+obsOmega))
-                        #    y.append(i*step * math.cos(omega2_min+obsOmega))
+                        #    x.append(i*step * math.sin(omega2_min+posangle_obs))
+                        #    y.append(i*step * math.cos(omega2_min+posangle_obs))
                         #plt.plot(x, y, 'k-.')
                         #x    = []
                         #y    = []
                         #for i in range(n+1):
-                        #    x.append(i*step * math.sin(omega2_max+obsOmega))
-                        #    y.append(i*step * math.cos(omega2_max+obsOmega))
+                        #    x.append(i*step * math.sin(omega2_max+posangle_obs))
+                        #    y.append(i*step * math.cos(omega2_max+posangle_obs))
                         #plt.plot(x, y, 'k-.')
                         n    = 100
                         x    = []
@@ -371,8 +359,8 @@ def show_response(centre, semimajor, semiminor, posang, obsDir, pntDir, rhos):
                         step = (omega2_max - omega2_min)/n
                         for i in range(n+1):
                             radians = i*step + omega2_min
-                            cos_radians = math.sin(radians+obsOmega)
-                            sin_radians = math.cos(radians+obsOmega)
+                            cos_radians = math.sin(radians+posangle_obs)
+                            sin_radians = math.cos(radians+posangle_obs)
                             x.append(rho * cos_radians)
                             y.append(rho * sin_radians)
                         plt.plot(x, y, 'k-')
@@ -381,8 +369,8 @@ def show_response(centre, semimajor, semiminor, posang, obsDir, pntDir, rhos):
         #x = []
         #y = []
         #for i in range(10):
-        #    x.append(i*0.1 * math.sin(0.0+obsOmega))
-        #    y.append(i*0.1 * math.cos(0.0+obsOmega))
+        #    x.append(i*0.1 * math.sin(0.0+posangle_obs))
+        #    y.append(i*0.1 * math.cos(0.0+posangle_obs))
         #plt.plot(x, y, 'k-')
 
         # Build circle
@@ -425,6 +413,9 @@ if __name__ == '__main__':
         print("Matplotlib is not installed on your system. Abort.")
         sys.exit()
 
+    # Set Psf size
+    delta_max = 0.4
+
     # Set model parameters
     centre = gammalib.GSkyDir()
     centre.radec_deg(0.0, 0.0)
@@ -434,7 +425,8 @@ if __name__ == '__main__':
 
     # Set observed photon direction
     obsDir = gammalib.GSkyDir()
-    obsDir.radec_deg(0.4, -0.3)
+    #obsDir.radec_deg(0.4, -0.3)
+    obsDir.radec_deg(-0.4, -0.3)
 
     # Set pointing direction
     pntDir = gammalib.GSkyDir()
@@ -444,5 +436,5 @@ if __name__ == '__main__':
     rhos = [i*0.02 for i in range(51)]
 
     # Show elliptical response geometry
-    show_response(centre, semimajor, semiminor, posang, obsDir, pntDir, rhos)
+    show_response(centre, semimajor, semiminor, posang, obsDir, pntDir, delta_max, rhos)
     
