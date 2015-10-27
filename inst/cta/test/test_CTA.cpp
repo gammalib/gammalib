361032e1cd7afa66137edfacaa999eedaaa2fb44
Jürgen Knödlseder <jurgen.knodlseder@irap.omp.eu>
2015-10-27 22:04:13 +0100

    Merge in code.

--- inst/cta/test/test_CTA.cpp
+++ inst/cta/test/test_CTA.cpp
@@ -389,27 +389,16 @@ void TestGCTAResponse::test_response_psf(void)
             sum += rsp.psf(r * gammalib::deg2rad, 0.0, 0.0, 0.0, 0.0, eng.log10TeV()) *
                    gammalib::twopi * std::sin(r * gammalib::deg2rad) * dr *
                    gammalib::deg2rad;
-            //sum += rsp.psf(r * gammalib::deg2rad, 0.0, 0.0, 0.0, 0.0, eng.log10TeV()) *
-            //       gammalib::twopi * r * gammalib::deg2rad;
             
             // since 'sum' already totals to 1.0, its also a 'fraction',
             // which we can plug back into containment_radius(), to compare
             // with the origial radius 'r'
-            if ( sum < 1.0 && sum > 0.0 ) {
-              rcont = rsp.psf()->containment_radius(sum, eng.log10TeV());
-              char percent[50] ;
-              sprintf( percent, "%8.5f", sum*100.0 ) ;
-              test_value( rcont, r*gammalib::deg2rad, 0.1, "PSF containment radius for "+eng.print()+" and " + percent + "% containment");
+            if (sum < 1.0 && sum > 0.0) {
+                rcont = rsp.psf()->containment_radius(sum, eng.log10TeV());
+                char percent[50] ;
+                sprintf(percent, "%8.5f", sum*100.0) ;
+                test_value(rcont, r*gammalib::deg2rad, 0.1, "PSF containment radius for "+eng.print()+" and " + percent + "% containment");
             }
-            //std::cout << "psf | en=" << eng.print() << "  r=" << r << "  rcont=" << rcont << "  sum=" << sum << std::endl;
-            //char status[30] = "" ;
-            //if ( fabs(r-rcont) > 0.006 ) {
-            //  sprintf( status, "fail!" ) ;
-            //}
-            //else {
-            //  sprintf( status, "ok" ) ;
-            //}
-            //printf( "psf_gauss | en=%5.1f  r=%7.5f  rcont=%9.3e  fabs(r-rcont)=%9.3e  sum=%9.3e  %5s\n", eng.TeV(), r, rcont, fabs(r-rcont), sum, status ) ;
         }
         test_value(sum, 1.0, 0.001, "PSF integration for "+eng.print());
         
