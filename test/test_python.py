d5634a6615f58f235fc3a0926e5e0518db9849a8
Jürgen Knödlseder <jurgen.knodlseder@irap.omp.eu>
2016-06-25 10:50:01 +0200

    Remove temporary Python directory even if it's not empty

--- test/test_python.py
+++ test/test_python.py
@@ -207,7 +207,7 @@ def test(installed=False):
 
     # Remove temporary direction
     if installed:
-        os.rmdir(path)
+        os.system('rm -f %s' % (path))
 
     # Exit with return code
     sys.exit(rc)
