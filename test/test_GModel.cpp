f89f94b3f92d0593fe2faefdb48a3a2ec446ceaf
Domenico Tiziani <domenico.tiziani@fau.de>
2016-10-26 16:53:29 +0200

    Added unit tests for spatial model component scales

--- test/test_GModel.cpp
+++ test/test_GModel.cpp
@@ -1433,6 +1433,9 @@ void TestGModel::test_spatial_composite(void)
     GModelSpatialComposite model2(*element);
     test_value(model2.size(), 5);
     test_value(model2.type(), "Composite", "Check model type");
+    test_value(model2.component_scale(0), 1.0);
+    test_value(model2.component_scale(1), 3.0);
+    test_value(model2.sum_of_scales(), 4.0);
       
     // Test access of individual parameters
     test_value(model2["2:RA"].value(), 83.6331);
