6213fdcd60bb2264b5f6105933b26444ca5eacad
Josh Cardenzana <jcardenzana@irap.omp.eu>
2018-04-27 10:54:13 +0200

    Updated GModelSpatialRadialProfileGauss (#2463) Add constructor from GSkyDir and sigma values Add setter/getter for sigma parameter

--- src/model/GModelSpatialRadialProfileGauss.cpp
+++ src/model/GModelSpatialRadialProfileGauss.cpp
@@ -98,6 +98,31 @@ GModelSpatialRadialProfileGauss::GModelSpatialRadialProfileGauss(const GXmlEleme
 
 
 /***********************************************************************//**
+ * @brief Constructor
+ *
+ * @param[in] dir Sky position of Gaussian.
+ * @param[in] sigma Width of Gaussian (in degrees).
+ *
+ * Constructs a Gaussian spatial model using a sky direction (@p dir) and
+ * a Gaussian width parameter @p sigma in degrees.
+ ***************************************************************************/
+GModelSpatialRadialProfileGauss::GModelSpatialRadialProfileGauss(const GSkyDir& dir,
+                                                                 const double&  sigma) :
+                                 GModelSpatialRadialProfile()
+{
+    // Initialise members
+    init_members();
+
+    // Assign direction and sigma
+    this->dir(dir);
+    this->sigma(sigma);
+
+    // Return
+    return;
+}
+
+
+/***********************************************************************//**
  * @brief Copy constructor
  *
  * @param[in] model Radial Gaussian profile model.
