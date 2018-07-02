684e930fbbb408c1e902e48ea6f4ac07a9930a02
Jürgen Knödlseder <jurgen.knodlseder@irap.omp.eu>
2018-07-02 14:21:22 +0200

    Support pickling of MWL classes (#1938)

--- inst/lat/test/test_LAT.py
+++ inst/lat/test/test_LAT.py
@@ -1,7 +1,7 @@
 # ==========================================================================
 # This module performs unit tests for the GammaLib LAT module.
 #
-# Copyright (C) 2012-2017 Juergen Knoedlseder
+# Copyright (C) 2012-2018 Juergen Knoedlseder
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -87,6 +87,38 @@ class Test(gammalib.GPythonTestSuite):
         # Return
         return
 
+    # Test class pickeling
+    def _test_pickeling(self):
+        """
+        Test class pickeling
+        """
+        # Perform pickeling tests of empty classes
+        #test_support._pickeling(self, gammalib.GLATAeff())
+        #test_support._pickeling(self, gammalib.GLATEdisp())
+        #test_support._pickeling(self, gammalib.GLATEfficiency())
+        #test_support._pickeling(self, gammalib.GLATEventAtom())
+        #test_support._pickeling(self, gammalib.GLATEventBin())
+        #test_support._pickeling(self, gammalib.GLATEventCube())
+        #test_support._pickeling(self, gammalib.GLATEventList())
+        #test_support._pickeling(self, gammalib.GLATInstDir())
+        #test_support._pickeling(self, gammalib.GLATLtCube())
+        #test_support._pickeling(self, gammalib.GLATLtCubeMap())
+        #test_support._pickeling(self, gammalib.GLATMeanPsf())
+        #test_support._pickeling(self, gammalib.GLATObservation())
+        #test_support._pickeling(self, gammalib.GLATPsf())
+        #test_support._pickeling(self, gammalib.GLATPsfV1())
+        #test_support._pickeling(self, gammalib.GLATPsfV3())
+        #test_support._pickeling(self, gammalib.GLATResponse())
+        #test_support._pickeling(self, gammalib.GLATResponseTable())
+        #test_support._pickeling(self, gammalib.GLATRoi())
+
+        # Setup test
+
+        # Perform pickeling tests of filled classes
+
+        # Return
+        return
+
     # Set test functions
     def set(self):
         """
@@ -98,6 +130,7 @@ class Test(gammalib.GPythonTestSuite):
         # Append tests
         self.append(self._test_eventlist_access, 'Test GLATEventList event access')
         self.append(self._test_eventlist_slicing, 'Test GLATEventList slicing')
+        self.append(self._test_pickeling, 'Test LAT class pickeling')
 
         # Return
         return
