a08a3dd52390a6fdf722678f9cbed9eb6b5d456c
Jürgen Knödlseder <jurgen.knodlseder@irap.omp.eu>
2019-09-30 14:22:17 +0200

    Update change log

--- include/GSkyMap.hpp
+++ include/GSkyMap.hpp
@@ -1,7 +1,7 @@
 /***************************************************************************
  *                       GSkyMap.hpp - Sky map class                       *
  * ----------------------------------------------------------------------- *
- *  copyright (C) 2010-2018 by Juergen Knoedlseder                         *
+ *  copyright (C) 2010-2019 by Juergen Knoedlseder                         *
  * ----------------------------------------------------------------------- *
  *                                                                         *
  *  This program is free software: you can redistribute it and/or modify   *
